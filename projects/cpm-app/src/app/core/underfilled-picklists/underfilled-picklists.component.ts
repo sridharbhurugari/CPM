1d6c81045542853a5276a1eb89eb184423c9585a
Gregory Bricker <Gregory.Bricker@omnicell.com>
2019-10-16 17:55:38 -0400

    Update under-filled picklists text style.  Update scrollbar style.

--- projects/cpm-app/src/app/core/underfilled-picklists/underfilled-picklists.component.ts
+++ projects/cpm-app/src/app/core/underfilled-picklists/underfilled-picklists.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, AfterViewInit, AfterViewChecked } from '@angular/core';
 import { UnderfilledPicklist } from '../model/underfilled-picklist';
+import { WindowRef } from '../../services/window-ref';
 
 @Component({
   selector: 'app-underfilled-picklists',
@@ -7,10 +8,19 @@ import { UnderfilledPicklist } from '../model/underfilled-picklist';
   styleUrls: ['./underfilled-picklists.component.scss']
 })
 export class UnderfilledPicklistsComponent {
+  _picklists: UnderfilledPicklist[];
 
   @Input()
-  picklists: UnderfilledPicklist[];
+  set picklists(value: UnderfilledPicklist[]){
+    this._picklists = value;
+    if(this.windowRef.nativeWindow){
+      this.windowRef.nativeWindow.dispatchEvent(new Event('resize'));
+    }
+  }
+  
+  get picklists(): UnderfilledPicklist[]{
+    return this._picklists;
+  }
 
-  constructor() { }
-
+  constructor(private windowRef: WindowRef) { }
 }
