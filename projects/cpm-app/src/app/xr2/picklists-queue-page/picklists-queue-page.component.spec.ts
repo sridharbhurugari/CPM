45b431684af2bf9dacab0e0a2f41b9935b87f6a5
dave molesky <dmolesky04@gmail.com>
2019-11-22 09:18:42 -0500

    Fix unit tests

--- projects/cpm-app/src/app/xr2/picklists-queue-page/picklists-queue-page.component.spec.ts
+++ projects/cpm-app/src/app/xr2/picklists-queue-page/picklists-queue-page.component.spec.ts
@@ -1,6 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PicklistsQueuePageComponent } from './picklists-queue-page.component';
+import { PicklistsQueueComponent } from './../picklists-queue/picklists-queue.component';
+import { PicklistsQueueService } from '../../api-xr2/services/picklists-queue.service';
+import { of, Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
+import { MockTranslatePipe } from '../../core/testing/mock-translate-pipe.spec';
+import { Input, Component } from '@angular/core';
+import { MockSearchPipe } from '../../core/testing/mock-search-pipe.spec';
+import { ButtonActionModule, GridModule } from '@omnicell/webcorecomponents';
+
+@Component({
+  selector: 'oc-search-box',
+  template: ''
+})
+
+class MockSearchBox {
+  searchOutput$: Observable<string> = of();
+  @Input()placeHolderText: string;
+}
 
 describe('PicklistsQueuePageComponent', () => {
   let component: PicklistsQueuePageComponent;
@@ -8,7 +27,13 @@ describe('PicklistsQueuePageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PicklistsQueuePageComponent ]
+      declarations: [ PicklistsQueuePageComponent, PicklistsQueueComponent, MockTranslatePipe, MockSearchBox , MockSearchPipe],
+      imports: [GridModule, ButtonActionModule],
+      providers: [
+        { provide: PicklistsQueueService, useValue: { get: () => of([]) } },
+        { provide: ActivatedRoute, useValue: { snapshot: { queryParamMap : { get: () => '' } } } },
+        { provide: WpfActionControllerService, useVaule: { } }
+      ]
     })
     .compileComponents();
   }));
--- projects/cpm-app/src/app/xr2/picklists-queue/picklists-queue.component.spec.ts
+++ projects/cpm-app/src/app/xr2/picklists-queue/picklists-queue.component.spec.ts
@@ -1,6 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PicklistsQueueComponent } from './picklists-queue.component';
+import { GridModule, SearchBoxComponent, ButtonActionModule } from '@omnicell/webcorecomponents';
+import { MockTranslatePipe } from '../../core/testing/mock-translate-pipe.spec';
+import { MockSearchPipe } from '../../core/testing/mock-search-pipe.spec';
+import { Observable, of } from 'rxjs';
+import { Input, Component } from '@angular/core';
+
+@Component({
+  selector: 'oc-search-box',
+  template: ''
+})
+
+class MockSearchBox {
+  searchOutput$: Observable<string> = of();
+  @Input()placeHolderText: string;
+}
 
 describe('PicklistsQueueComponent', () => {
   let component: PicklistsQueueComponent;
@@ -8,7 +23,8 @@ describe('PicklistsQueueComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PicklistsQueueComponent ]
+      declarations: [ PicklistsQueueComponent, MockTranslatePipe, MockSearchPipe, MockSearchBox ],
+      imports: [GridModule, ButtonActionModule]
     })
     .compileComponents();
   }));
