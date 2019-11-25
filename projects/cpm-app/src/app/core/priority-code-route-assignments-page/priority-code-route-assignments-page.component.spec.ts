8e7cd3e319e435747629a5dc4c32c37fdc1a06b3
warren <warren.junker@omnicell.com>
2019-11-25 13:27:23 -0500

    Try adding Router and Location for vague error in testing

--- projects/cpm-app/src/app/core/priority-code-route-assignments-page/priority-code-route-assignments-page.component.spec.ts
+++ projects/cpm-app/src/app/core/priority-code-route-assignments-page/priority-code-route-assignments-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Location, CommonModule } from '@angular/common';
 import { PriorityCodeRouteAssignmentsPageComponent } from './priority-code-route-assignments-page.component';
 import { PriorityCodeRouteAssignmentsService } from '../../api-core/services/priority-code-route-assignments.service';
 import { of, fromEventPattern } from 'rxjs';
@@ -14,7 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { FormStatusImageModule } from '@omnicell/webcorecomponents/esm5/lib/form-status-image/form-status-image.module';
 import { GridModule } from '@omnicell/webcorecomponents';
 import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-priority-code-route-assignments',
@@ -27,8 +27,13 @@ class MockPriorityCodeRouteAssignmentsComponent {
 describe('PriorityCodeRouteAssignmentsPageComponent', () => {
   let component: PriorityCodeRouteAssignmentsPageComponent;
   let fixture: ComponentFixture<PriorityCodeRouteAssignmentsPageComponent>;
+  var router, location
 
   beforeEach(async(() => {
+    router = { navigate: () => { } };
+    spyOn(router, 'navigate');
+    location = { back: () => { } };
+    spyOn(location, 'back');
     TestBed.configureTestingModule({
       declarations: [ PriorityCodeRouteAssignmentsPageComponent, MockPriorityCodeRouteAssignmentsComponent,
          MockTranslatePipe, PickRouteSelectComponent, DeviceSequenceOrderComponent ],
@@ -36,11 +41,13 @@ describe('PriorityCodeRouteAssignmentsPageComponent', () => {
         { provide: PriorityCodeRouteAssignmentsService, useValue: { get: () => of([]) } },
         { provide: PriorityCodeRouteAssignmentsService, useValue: { getRoutes: () => of() } },
         { provide: ActivatedRoute, useValue: { snapshot: { queryParamMap : { get: () => '' } } } },
-        { provide: WpfActionControllerService, useVaule: { } }
+        { provide: WpfActionControllerService, useVaule: { } },
+        { provide: Location, useValue: location },
+        { provide: Router, useValue: router }
       ],
       imports: [
         SharedModule, FooterModule, LayoutModule, ButtonActionModule,
-        FormStatusImageModule, FormsModule, GridModule
+        FormStatusImageModule, FormsModule, GridModule, CommonModule
       ]
     })
     .overrideComponent(PriorityCodeRouteAssignmentsPageComponent, {
