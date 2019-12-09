ded3a794257f6084cdfc7a8bf72659d6782e77f9
Jeff Fircak <jeff.fircak@omnicell.com>
2019-12-09 15:38:00 -0500

    Fixed up test, more to come

--- projects/cpm-app/src/app/core/guidedinvmgmt-devicelist-page/guidedinvmgmt-devicelist-page.component.spec.ts
+++ projects/cpm-app/src/app/core/guidedinvmgmt-devicelist-page/guidedinvmgmt-devicelist-page.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GuidedInvMgmtDevicelistPageComponent } from './guidedinvmgmt-devicelist-page.component';
+import { MockTranslatePipe } from '../testing/mock-translate-pipe.spec';
+import { GridModule, FooterModule, LayoutModule, SvgIconModule, SearchModule } from '@omnicell/webcorecomponents';
+import { SharedModule } from '../../shared/shared.module';
 
 describe('GuidedinvmgmtDevicelistPageComponent', () => {
   let component: GuidedInvMgmtDevicelistPageComponent;
@@ -8,7 +11,8 @@ describe('GuidedinvmgmtDevicelistPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GuidedInvMgmtDevicelistPageComponent ]
+      declarations: [ GuidedInvMgmtDevicelistPageComponent, MockTranslatePipe ],
+      imports: [GridModule, FooterModule, LayoutModule, SearchModule, SvgIconModule, SharedModule]
     })
     .compileComponents();
   }));
