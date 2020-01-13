91a208b2fe1adce99e200cc3a7a05551ab5a32ae
Philip Paolo <philip.paolo@omnicell.com>
2020-01-13 08:32:11 -0500

    branch 'fe-240658-guide-cycle-count

--- projects/cpm-app/src/app/core/guidedinvmgmt-cyclecount-page/guidedinvmgmt-cyclecount-page.component.ts
+++ projects/cpm-app/src/app/core/guidedinvmgmt-cyclecount-page/guidedinvmgmt-cyclecount-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { map, shareReplay, filter, single, pluck } from 'rxjs/operators';
+import { map, shareReplay, filter, single, pluck, count } from 'rxjs/operators';
 import * as _ from 'lodash';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, of, forkJoin } from 'rxjs';
@@ -8,6 +8,8 @@ import { InputsModule } from '@omnicell/webcorecomponents';
 import { IGuidedCycleCount } from '../../api-core/data-contracts/i-guided-cycle-count';
 import { GuidedCycleCountService } from '../../api-core/services/guided-cycle-count-service';
 import { GuidedCycleCount } from '../model/guided-cycle-count';
+import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
+import { GuidedCycleCountUpdate } from '../model/guided-cycle-count-update';
 
 
 @Component({
@@ -18,18 +20,49 @@ import { GuidedCycleCount } from '../model/guided-cycle-count';
 
 export class GuidedInvMgmtCycleCountPageComponent implements OnInit, AfterViewInit {
   @ViewChild('numericEle', null) numericElement: NumericComponent;  
-  displayCycleCountItem: Observable<IGuidedCycleCount>;
+  displayCycleCountItem: IGuidedCycleCount;
+  cycleCountItems: Observable<IGuidedCycleCount[]>;
+  isLastItem: boolean;
 
   titleHeader = '\'GUIDED_CYCLE_COUNT\' | translate';
   route: any;
   constructor(    
     private activatedRoute: ActivatedRoute,
-    private guidedCycleCountService: GuidedCycleCountService
+    private guidedCycleCountService: GuidedCycleCountService,
+    private wpfActionController: WpfActionControllerService
     ) { }
+
+    mockData = [
+      new GuidedCycleCount({
+        DeviceLocationId: 86,  
+        ItemId: "ace500t",
+        BrandNameFormatted: "Tylenol 500mg tab",
+        GenericNamdFOrmatted: "acetaminophen 500mg tab",
+        ParLevel: 60,
+        ReorderLevel: 30,
+        ExpirationDate: new Date(),
+        LocationDescription: "Carosel 01-01-01",
+        QuantityOnHand: 55,
+        ReorderSource: "Internal"
+      }),
+      new GuidedCycleCount({
+        DeviceLocationId: 87,  
+        ItemId: "ace325t",
+        BrandNameFormatted: "Tylenol 325mg tab",
+        GenericNamdFOrmatted: "acetaminophen 325mg tab",
+        ParLevel: 60,
+        ReorderLevel: 30,
+        ExpirationDate: new Date(),
+        LocationDescription: "Carosel 01-01-02",
+        QuantityOnHand: 52,
+        ReorderSource: "Internal"
+      }),
+    ];
 
   ngOnInit() {
     var deviceId = this.activatedRoute.snapshot.queryParamMap.get('deviceId');
 
+<<<<<<< HEAD
   //  this.displayCycleCountItem = this.guidedCycleCountService.get(deviceId).pipe(map(guidedCycleCountItems => {
   //    return guidedCycleCountItems.map(p => new GuidedCycleCount(p))[0];
   
@@ -48,12 +81,58 @@ export class GuidedInvMgmtCycleCountPageComponent implements OnInit, AfterViewIn
   }));
 
    //}));
+=======
+    //this.cycleCountItems = this.guidedCycleCountService.get(deviceId).pipe(map(guidedCycleCountItems => {
+    //  
+    //  return guidedCycleCountItems.map(p => new GuidedCycleCount(p));
+    //}));
+    
+    this.cycleCountItems = of(this.mockData);
+    this.nextRecord();
+>>>>>>> 8df17b92cc32e2088cddf49f21156b27ccfbf3b3
   }
 
   ngAfterViewInit() {
   }
 
   navigateBack(){
+    this.wpfActionController.ExecuteBackAction();
   }
 
+  navigateContinue(){
+    var updateSucceeded = false;
+
+    let update = new GuidedCycleCountUpdate({
+      DeviceLocationId: this.displayCycleCountItem.DeviceLocationId,
+      ItemId: this.displayCycleCountItem.ItemId,
+      ExpirationData: this.displayCycleCountItem.ExpirationDate,
+      QuantityOnHand: this.displayCycleCountItem.QuantityOnHand
+        });
+
+    this.guidedCycleCountService.post(update).pipe(map(success => {
+      updateSucceeded = success;
+      return success;
+    }));
+
+
+    if(this.isLastItem){
+      this.wpfActionController.ExecuteContinueAction();
+    }else{
+      this.nextRecord();
+    }
+  }
+
+  nextRecord(){
+    var itemCount = 0;
+    this.cycleCountItems.subscribe(x => {
+      this.displayCycleCountItem = x[0];
+      x.splice(0, 1);
+      itemCount = x.length;
+
+    });
+
+    if(itemCount == 0){
+      this.isLastItem = true;
+    }
+  }
 }
