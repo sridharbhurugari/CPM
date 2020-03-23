e4a180972b0aa1ddc0046ea67e7bf4158f988cfb
Raju Irla <rajui@omnicell.com>
2020-03-23 17:08:51 +0530

    Fixing the wrong component reference

--- projects/cpm-app/src/app/api-core/data-contracts/guided-cycle-count-update.spec.ts
+++ projects/cpm-app/src/app/api-core/data-contracts/guided-cycle-count-update.spec.ts
@@ -1,7 +1,9 @@
-import { GuidedCycleCountUpdate } from './guided-cycle-count-update';
+import { deviceCycleCountItemUpdate } from './guided-cycle-count-update';
+import { TestBed } from '@angular/core/testing';
 
 describe('GuidedCycleCountUpdate', () => {
   it('should create an instance', () => {
-    expect(new GuidedCycleCountUpdate()).toBeTruthy();
+    const component:deviceCycleCountItemUpdate = TestBed.get(deviceCycleCountItemUpdate);
+    expect(component).toBeTruthy();
   });
 });
