a7fd3cc73b4a310cc3860f99e0a133960b84603a
Jeff Fircak <>
2020-03-23 11:41:32 -0400

    Fixed up rest of tests

--- projects/cpm-app/src/app/core/guidedinvmgmt-cyclecount-page/guidedinvmgmt-cyclecount-page.component.spec.ts
+++ projects/cpm-app/src/app/core/guidedinvmgmt-cyclecount-page/guidedinvmgmt-cyclecount-page.component.spec.ts
@@ -4,7 +4,10 @@ import { GuidedInvMgmtCycleCountPageComponent } from './guidedinvmgmt-cyclecount
 import { SharedModule } from '../../shared/shared.module';
 import { MockTranslatePipe } from '../testing/mock-translate-pipe.spec';
 import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
-import { DatepickerComponent } from '@omnicell/webcorecomponents';
+import { InputsModule, LayoutModule, DatepickerModule, FooterModule, ButtonActionModule} from '@omnicell/webcorecomponents';
+import { ActivatedRoute, ActivatedRouteSnapshot, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
+import { GuidedCycleCountService } from '../../api-core/services/guided-cycle-count-service';
 
 describe('GuidedInvMgmtCycleCountPageComponent', () => {
   let component: GuidedInvMgmtCycleCountPageComponent;
@@ -12,10 +15,12 @@ describe('GuidedInvMgmtCycleCountPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GuidedInvMgmtCycleCountPageComponent, MockTranslatePipe, DatepickerComponent ],
-      imports: [],
+      declarations: [ GuidedInvMgmtCycleCountPageComponent, MockTranslatePipe ],
+      imports: [SharedModule, LayoutModule, DatepickerModule, FooterModule, ButtonActionModule, InputsModule ],
       providers: [
-        { provide: WpfActionControllerService, useValue: { } }
+        { provide: WpfActionControllerService, useValue: { } },
+        { provide: ActivatedRoute, useValue: { snapshot: { queryParamMap : { get: () => '' } } } },
+        { provide: GuidedCycleCountService, useValue: { get: () => of([]) } },
       ]
     })
     .compileComponents();
