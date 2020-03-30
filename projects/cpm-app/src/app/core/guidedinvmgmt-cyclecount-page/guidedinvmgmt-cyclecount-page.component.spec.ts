30ff4a7390d804a13f59c36bd1412de442981266
Raju Irla <rajui@omnicell.com>
2020-03-30 14:24:52 +0530

    Added translation strings and removed pop up dialog

---
+++ projects/cpm-app/src/app/core/guidedinvmgmt-cyclecount-page/guidedinvmgmt-cyclecount-page.component.spec.ts
@@ -0,0 +1,40 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GuidedInvMgmtCycleCountPageComponent } from './guidedinvmgmt-cyclecount-page.component';
+import { SharedModule } from '../../shared/shared.module';
+import { MockTranslatePipe } from '../testing/mock-translate-pipe.spec';
+import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
+import { InputsModule, LayoutModule, DatepickerModule, FooterModule, ButtonActionModule} from '@omnicell/webcorecomponents';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { GuidedCycleCountService } from '../../api-core/services/guided-cycle-count-service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+describe('GuidedInvMgmtCycleCountPageComponent', () => {
+  let component: GuidedInvMgmtCycleCountPageComponent;
+  let fixture: ComponentFixture<GuidedInvMgmtCycleCountPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GuidedInvMgmtCycleCountPageComponent, MockTranslatePipe ],
+      imports: [SharedModule, LayoutModule, DatepickerModule, FooterModule, ButtonActionModule, InputsModule, FormsModule, ReactiveFormsModule ],
+      providers: [
+        { provide: WpfActionControllerService, useValue: { } },
+        { provide: ActivatedRoute, useValue: { snapshot: { queryParamMap : { get: () => '' } } } },
+        { provide: GuidedCycleCountService, useValue: { get: () => of([]) } },
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GuidedInvMgmtCycleCountPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
---
+++ projects/cpm-app/src/app/core/guidedinvmgmt-cyclecount-page/guidedinvmgmt-cyclecount-page.component.ts
@@ -0,0 +1,129 @@
+import { Component, OnInit, Output, EventEmitter,AfterViewInit, ViewChild } from '@angular/core';
+import { map, shareReplay, filter, single, pluck, count } from 'rxjs/operators';
+import * as _ from 'lodash';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of, forkJoin } from 'rxjs';
+import { NumericComponent, DatepickerComponent } from '@omnicell/webcorecomponents';
+import { IGuidedCycleCount } from '../../api-core/data-contracts/i-guided-cycle-count';
+import { GuidedCycleCountService } from '../../api-core/services/guided-cycle-count-service';
+import { GuidedCycleCount } from '../model/guided-cycle-count';
+import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
+import { deviceCycleCountItemUpdate } from '../../api-core/data-contracts/guided-cycle-count-update';
+
+
+@Component({
+  selector: 'app-guidedinvmgmt-cyclecount-page',
+  templateUrl: './guidedinvmgmt-cyclecount-page.component.html',
+  styleUrls: ['./guidedinvmgmt-cyclecount-page.component.scss']
+})
+
+export class GuidedInvMgmtCycleCountPageComponent implements OnInit, AfterViewInit {
+  @ViewChild('numericEle', null) numericElement: NumericComponent;
+  @ViewChild(DatepickerComponent, null) datepicker: DatepickerComponent;
+  displayCycleCountItem: IGuidedCycleCount;
+  cycleCountItems: Observable<IGuidedCycleCount[]>;
+  cycleCountItemsCopy: IGuidedCycleCount[];
+  itemCount: number;
+  isLastItem: boolean;
+  currentItemCount: number;
+  public time: Date = new Date();
+  titleHeader = '\'GUIDED_CYCLE_COUNT\' | translate';
+  route: any;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private guidedCycleCountService: GuidedCycleCountService,
+    private wpfActionController: WpfActionControllerService
+  ) {
+    setInterval(() => {
+      this.time = new Date();
+    }, 1);
+    this.itemCount = 0;
+    this.currentItemCount = 0;
+  }
+
+
+  ngOnInit() {
+    var deviceId = this.activatedRoute.snapshot.queryParamMap.get('deviceId');
+    this.getCycleCountData(deviceId);
+  }
+
+  getCycleCountData(deviceID) {
+    this.cycleCountItems = this.guidedCycleCountService.get(deviceID).pipe(map(guidedCycleCountItems => {
+      return guidedCycleCountItems.map(p => new GuidedCycleCount(p));
+    }));
+    this.cycleCountItems.subscribe(x => {
+      if (x.length > 0 && x[0].ExpirationDate) {
+        this.displayCycleCountItem = x[0];
+        var date = new Date(x[0].ExpirationDate);
+        this.displayCycleCountItem.ExpirationDateFormatted = ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + ((date.getFullYear() == 1) ? 1900 : date.getFullYear());
+        this.cycleCountItemsCopy = x;
+        x.splice(0,1);
+        this.itemCount = x.length + 1;
+      }
+      if (this.itemCount <= 1) {
+        this.isLastItem = true;
+      }
+      else {
+        this.isLastItem = false;
+      }
+      this.currentItemCount++;
+    });
+  }
+
+  showValidComponent() {
+    return this.displayCycleCountItem && this.cycleCountItemsCopy.length > 1;
+  }
+
+  ngAfterViewInit() {
+  }
+
+  FormatExpireDate(date: Date) {
+    if (date) {
+      var date = new Date(date);
+      return ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + ((date.getFullYear() == 1) ? 1900 : date.getFullYear());
+    }
+  }
+
+  navigateBack() {
+    this.wpfActionController.ExecuteBackAction();
+  }
+
+  navigateContinue() {
+    if (this.displayCycleCountItem != null) {
+      let update = new deviceCycleCountItemUpdate({
+        DeviceLocationId: this.displayCycleCountItem.DeviceLocationId,
+        ItemId: this.displayCycleCountItem.ItemId,
+        ExpirationDate: new Date(this.displayCycleCountItem.ExpirationDateFormatted),
+        QuantityOnHand: this.displayCycleCountItem.QuantityOnHand
+      });
+
+      var deviceId = this.activatedRoute.snapshot.queryParamMap.get('deviceId');
+
+      this.guidedCycleCountService.post(deviceId, update).subscribe(
+        res => {
+          console.log(res);
+        }
+      );
+    }
+    if (this.isLastItem || this.currentItemCount == this.itemCount) {
+      this.wpfActionController.ExecuteBackAction();
+    } else {
+      this.nextRecord();
+    }
+  }
+
+  nextRecord() {
+    if (this.itemCount == 0) {
+      this.isLastItem = true;
+    }
+    else {
+      this.displayCycleCountItem = this.cycleCountItemsCopy[this.currentItemCount - 1];
+      var date = new Date(this.cycleCountItemsCopy[this.currentItemCount - 1].ExpirationDate);
+      this.displayCycleCountItem.ExpirationDateFormatted = ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + ((date.getFullYear() == 1) ? 1900 : date.getFullYear());
+      this.currentItemCount++;
+      if (this.currentItemCount == this.itemCount) {
+        this.isLastItem = true;
+      }
+    }
+  }
+}
