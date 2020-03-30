5eae61d1f0d6e85cb33dae0dc9bc1a3cbd7a8243
omnicell\josephr <joseph.ross@omnicell.com>
2020-03-30 10:45:00 -0400

    Swapped to using general SignalR connection

--- projects/cpm-app/src/app/xr2/services/event-connection.service.spec.ts
+++ projects/cpm-app/src/app/xr2/services/event-connection.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { EventConnectionService } from './event-connection.service';
 import { OalCoreModule, OcapHttpClientService } from 'oal-core';
 import { environment } from './../../../environments/environment';
+import { OcapUrlBuilderService } from '../../shared/services/ocap-url-builder.service';
 
 beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,6 +13,9 @@ beforeEach(() => {
             httpClientService: OcapHttpClientService,
             configEndpointKey: 'configEndpoint'
           }),
+        ],
+        providers: [
+          { provide: OcapUrlBuilderService, useValue: { buildUrl: () => {}} },
         ]
     });
 });
