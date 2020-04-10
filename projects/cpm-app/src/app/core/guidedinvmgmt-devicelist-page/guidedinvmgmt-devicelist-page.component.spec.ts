30f9520bda54304a62b83c1eb2d7827441b3b7da
omnicell\josephr <joseph.ross@omnicell.com>
2020-04-10 10:08:10 -0400

    Fixed testing setup

--- projects/cpm-app/src/app/core/guidedinvmgmt-devicelist-page/guidedinvmgmt-devicelist-page.component.spec.ts
+++ projects/cpm-app/src/app/core/guidedinvmgmt-devicelist-page/guidedinvmgmt-devicelist-page.component.spec.ts
@@ -7,6 +7,8 @@ import { SharedModule } from '../../shared/shared.module';
 import { GuidedDeviceListService } from '../../api-core/services/guided-device-list-service';
 import { of } from 'rxjs';
 import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
+import { TranslateService } from '@ngx-translate/core';
+import { HardwareLeaseService } from '../../api-core/services/hardware-lease-service';
 
 describe('GuidedinvmgmtDevicelistPageComponent', () => {
   let component: GuidedInvMgmtDevicelistPageComponent;
@@ -19,7 +21,9 @@ describe('GuidedinvmgmtDevicelistPageComponent', () => {
       providers: [
         { provide: GuidedDeviceListService, useValue: { get: () => of([]) } },
         { provide: WpfActionControllerService, useValue: { } },
-        { provide: PopupDialogService, useValue: {} }
+        { provide: PopupDialogService, useValue: {} },
+        { provide: TranslateService, useValue: {}},
+        { provide: HardwareLeaseService, useValue: {}},
       ]
     })
     .compileComponents();
