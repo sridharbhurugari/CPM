0d8ec8a46d8bc7d19613a3001826a1f9c088d7de
Brian Albaugh <Brian.Albaugh@omnicell.com>
2020-04-13 15:02:45 -0400

    undoing tests

--- projects/cpm-app/src/app/core/guidedinvmgmt-devicelist-page/guidedinvmgmt-devicelist-page.component.spec.ts
+++ projects/cpm-app/src/app/core/guidedinvmgmt-devicelist-page/guidedinvmgmt-devicelist-page.component.spec.ts
@@ -1,43 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { GuidedInvMgmtDevicelistPageComponent } from './guidedinvmgmt-devicelist-page.component';
 import { MockTranslatePipe } from '../testing/mock-translate-pipe.spec';
 import { GridModule, FooterModule, LayoutModule, SvgIconModule, SearchModule, ButtonActionModule, PopupDialogService } from '@omnicell/webcorecomponents';
 import { SharedModule } from '../../shared/shared.module';
 import { GuidedDeviceListService } from '../../api-core/services/guided-device-list-service';
-import { of, Observable } from 'rxjs';
+import { of } from 'rxjs';
 import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
-import { TranslateService } from '@ngx-translate/core';
-import { HardwareLeaseService } from '../../api-core/services/hardware-lease-service';
-import { LeaseVerificationResult } from '../../api-core/data-contracts/lease-verification-result';
-import { GuidedDeviceList } from '../model/guided-device-list';
-import { IGuidedDeviceList } from '../../api-core/data-contracts/i-guided-device-list';
+import { MockColHeaderSortable } from '../../shared/testing/mock-col-header-sortable.spec';
+import { MockAppHeaderContainer } from '../testing/mock-app-header.spec';
+import { MockSearchPipe } from '../testing/mock-search-pipe.spec';
 
 describe('GuidedinvmgmtDevicelistPageComponent', () => {
   let component: GuidedInvMgmtDevicelistPageComponent;
   let fixture: ComponentFixture<GuidedInvMgmtDevicelistPageComponent>;
-  let popupDialogService: Partial<PopupDialogService>;
-  let hardwareLeaseService: Partial<HardwareLeaseService>;
-  let guidedDeviceListserice: Partial<GuidedDeviceListService>;
-
-  let leaseVerificationResult: LeaseVerificationResult = 1;
-  let translateService: Partial<TranslateService>;
-  let devices: IGuidedDeviceList[] = [];
 
   beforeEach(async(() => {
-    popupDialogService = { showOnce: jasmine.createSpy('showOnce') };
-    hardwareLeaseService = { HasDeviceLease: () => of(leaseVerificationResult) };
-    translateService = { get: () => of('dummyText') };
-    guidedDeviceListserice = {get: () => of(devices) };
-
     TestBed.configureTestingModule({
-      declarations: [ GuidedInvMgmtDevicelistPageComponent, MockTranslatePipe ],
-      imports: [GridModule, FooterModule, LayoutModule, SearchModule, SvgIconModule, SharedModule, ButtonActionModule],
+      declarations: [ GuidedInvMgmtDevicelistPageComponent, MockTranslatePipe, MockColHeaderSortable, MockAppHeaderContainer, MockSearchPipe ],
+      imports: [GridModule, FooterModule, LayoutModule, SearchModule, SvgIconModule, ButtonActionModule],
       providers: [
-        { provide: GuidedDeviceListService, useValue: guidedDeviceListserice },
+        { provide: GuidedDeviceListService, useValue: { get: () => of([]) } },
         { provide: WpfActionControllerService, useValue: { } },
-        { provide: PopupDialogService, useValue: popupDialogService },
-        { provide: TranslateService, useValue: translateService },
-        { provide: HardwareLeaseService, useValue: hardwareLeaseService }
+        { provide: PopupDialogService, useValue: {} }
       ]
     })
     .compileComponents();
@@ -51,15 +36,5 @@ describe('GuidedinvmgmtDevicelistPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  describe('navigate', () => {
-    beforeEach(() => {
-      component.navigate(1);
-    });
-
-    it('should show popup for failure', () => {
-      expect(popupDialogService.showOnce).toHaveBeenCalled();
-    });
   });
 });
