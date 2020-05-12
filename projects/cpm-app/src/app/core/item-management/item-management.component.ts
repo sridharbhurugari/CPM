b43e3669679061ffcc141efae475ed647918eab3
dave molesky <dmolesky04@gmail.com>
2020-05-12 08:38:16 -0400

    Get search and sort working

--- projects/cpm-app/src/app/core/item-management/item-management.component.ts
+++ projects/cpm-app/src/app/core/item-management/item-management.component.ts
@@ -4,8 +4,11 @@ import { nameof } from '../../shared/functions/nameof';
 import { SearchBoxComponent } from '@omnicell/webcorecomponents';
 import { WindowService } from '../../shared/services/window-service';
 import { ItemManagementService } from '../../api-core/services/item-management.service';
-import { map, shareReplay } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { IColHeaderSortChanged } from '../../shared/events/i-col-header-sort-changed';
+import { Many } from 'lodash';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-item-management',
@@ -13,17 +16,18 @@ import { Observable } from 'rxjs';
   styleUrls: ['./item-management.component.scss']
 })
 
-export class ItemManagementComponent implements OnInit  {
+export class ItemManagementComponent implements OnInit, AfterViewInit  {
 
+  readonly itemIdPropertyName = nameof<ItemManagement>('ItemId');
+  readonly unitDoseQtyOnHandPropertyName = nameof<ItemManagement>('UnitDoseQtyOnHand');
+  readonly bulkQtyOnHandPropertyName = nameof<ItemManagement>('BulkQtyOnHand');
+  readonly totalQtyOnHandPropertyName = nameof<ItemManagement>('TotalQtyOnHand');
+  currentSortPropertyName: string = this.itemIdPropertyName;
   ItemManagements$: Observable<ItemManagement[]>;
-  @ViewChild('searchBox', {
-    static: true
-  })
-  searchElement: SearchBoxComponent;
 
+  @ViewChild('searchBox', null) searchElement: SearchBoxComponent;
   searchTextFilter: string;
-
-  searchFields = [nameof<ItemManagement>('ItemId'), nameof<ItemManagement>('ItemDescription')];
+  searchFields = [ this.itemIdPropertyName ];
 
   ngOnInit() {
     this.ItemManagements$ = this.itemManagementService.get().pipe(map(x => {
@@ -31,6 +35,29 @@ export class ItemManagementComponent implements OnInit  {
       return displayObjects;
     }), shareReplay(1));
   }
+
+  ngAfterViewInit() {
+    this.searchElement.searchOutput$
+      .pipe(
+        switchMap((searchData: string) => {
+          return of(searchData);
+        })
+      )
+      .subscribe(data => {
+        this.searchTextFilter = data;
+      });
+  }
+
+  columnSelected(event: IColHeaderSortChanged){
+    this.currentSortPropertyName = event.ColumnPropertyName;
+    this.ItemManagements$ = this.ItemManagements$.pipe(map(itemManagements => {
+      return this.sort(itemManagements, event.SortDirection);
+    }));
+  }
+
+  sort(itemManagements: ItemManagement[], sortDirection: Many<boolean|'asc'|'desc'>): ItemManagement[]{
+    return _.orderBy(itemManagements, x => x[this.currentSortPropertyName], sortDirection);
+}
 
   constructor(private windowService: WindowService, private itemManagementService: ItemManagementService) { }
 
