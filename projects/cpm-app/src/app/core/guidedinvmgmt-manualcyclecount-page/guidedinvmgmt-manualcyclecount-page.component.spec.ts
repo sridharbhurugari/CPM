f2401ff533332745ae4a3835b720a8c3cbab714e
Manoj Challa <manojc@omnicell.com>
2020-05-18 23:25:11 +0530

    testcases added

--- projects/cpm-app/src/app/core/guidedinvmgmt-manualcyclecount-page/guidedinvmgmt-manualcyclecount-page.component.spec.ts
+++ projects/cpm-app/src/app/core/guidedinvmgmt-manualcyclecount-page/guidedinvmgmt-manualcyclecount-page.component.spec.ts
@@ -212,11 +212,7 @@ describe('GuidedInvMgmtCycleCountPageComponent', () => {
       ).toHaveBeenCalled();
     });
   });
- // describe('cycle count data retrieval',()=>{
-   // it('cycle count data retrieval',() => {
-     // fixture.detectChanges();
-   // });
- //});
+ 
   describe('Sending proper date with required format', () => {
     it('return required date format', () => {
       var testDate = new Date('Tue Mar 31 2020 19:27:40 GMT+0530');
@@ -232,238 +228,6 @@ describe('GuidedInvMgmtCycleCountPageComponent', () => {
     });
   });
 
-//   describe('returns last item true', () => {
-//     it('return with is last item', () => {
-//       component.itemCount = 1;
-//       component.IsLastItem(); 
-//       expect(component.isLastItem).toBeTruthy();
-//     });
-//   });
-//   describe('returns last item false', () => {
-//     it('return with is last item', () => {
-//       component.itemCount = 3;
-//       component.IsLastItem(); 
-//       expect(component.isLastItem).toBeFalsy();
-//     });
-//   });
-  
-//   describe('returning false component data', () => {
-//     it('return proper valid component', () => {
-//       component.displayCycleCountItem = new GuidedManualCycleCountItemid({
-//         DeviceId: 5,
-//         DeviceDescription: 'carousel 2',
-//         DeviceLocationTypeId: '2023',
-//          ShelfNumber: 3,
-//          BinNumber: 2,
-//          SlotNumber: 1,
-//          DeviceLocationId: 86,  
-//          ItemId: "ace500t",
-//          BrandNameFormatted: "Tylenol 500mg tab",
-//          GenericNameFormatted: "acetaminophen 500mg tab",
-//         Units:"EA",
-//          ParLevel: 60,
-//          ReorderLevel: 30,
-//          ExpirationDate: new Date(),
-//          ExpirationDateFormatted: "10/03/2020",
-//          LocationDescription: "Carosel 01-01-01",
-//          QuantityOnHand: 55,
-//          ReorderSource: "Internal",
-//          ItmExpDateGranularity:"Month",
-//          QuantityMin:10,
-//          InStockQuantity:10,
-//          ItemDateFormat: "MM/DD/YYYY",
-//          PackageFormType: "B",
-//          PackageFormName: "Bulk",
-//          DrugId: "ACEtaminophen",
-//          ManufacturerName: "ARPON Labs",
-//       });
-//       var localcopy = [];
-//       component.cycleCountItemsCopy = localcopy;
-//       component.cycleCountItemsCopy.push(component.displayCycleCountItem);
-//       var validComp = component.showValidComponent();
-//       expect(validComp).toBeFalsy();
-//     });
-//   });
-//   describe('returning true component data', () => {
-//     it('return proper valid component', () => {
-//       component.displayCycleCountItem = new GuidedManualCycleCountItemid({
-//         DeviceId: 5,
-//         DeviceDescription: 'carousel 2',
-//         DeviceLocationTypeId: '2023',
-//          ShelfNumber: 3,
-//          BinNumber: 2,
-//          SlotNumber: 1,
-//          DeviceLocationId: 86,  
-//          ItemId: "ace500t",
-//          BrandNameFormatted: "Tylenol 500mg tab",
-//          GenericNameFormatted: "acetaminophen 500mg tab",
-//         Units:"EA",
-//          ParLevel: 60,
-//          ReorderLevel: 30,
-//          ExpirationDate: new Date(),
-//          ExpirationDateFormatted: "10/03/2020",
-//          LocationDescription: "Carosel 01-01-01",
-//          QuantityOnHand: 55,
-//          ReorderSource: "Internal",
-//          ItmExpDateGranularity:"Month",
-//          QuantityMin:10,
-//          InStockQuantity:10,
-//          ItemDateFormat: "MM/DD/YYYY",
-//          PackageFormType: "B",
-//          PackageFormName: "Bulk",
-//          DrugId: "ACEtaminophen",
-//          ManufacturerName: "ARPON Labs",
-//       });
-//       var localcopy = [];
-//       component.cycleCountItemsCopy = localcopy;
-//       component.cycleCountItemsCopy.push(component.displayCycleCountItem);
-//       component.cycleCountItemsCopy.push(component.displayCycleCountItem);
-//       component.cycleCountItemsCopy.push(component.displayCycleCountItem);
-//       var validComp = component.showValidComponent();
-//       expect(validComp).toBeTruthy();
-//     });
-//   });
-//   describe('next record with 0 records validation',()=>{
-//     it('returns the true if zero records',() => {
-//       component.itemCount = 0 ;
-//       component.nextRecord();
-//       var bReturn = component.isLastItem;
-//       expect(true).toBe(bReturn);
-//     });
-//   });
-//   describe('next record with more than 1 records validation',()=>{
-//     it('returns the true if zero records',() => {
-//       component.currentItemCount = 2;
-//       component.cycleCountItemsCopy = [];
-//       component.cycleCountItemsCopy.push(new GuidedManualCycleCountItemid({
-//         DeviceId: 5,
-//         DeviceDescription: 'carousel 2',
-//         DeviceLocationTypeId: '2023',
-//          ShelfNumber: 3,
-//          BinNumber: 2,
-//          SlotNumber: 1,
-//          DeviceLocationId: 86,  
-//          ItemId: "ace500t",
-//          BrandNameFormatted: "Tylenol 500mg tab",
-//          GenericNameFormatted: "acetaminophen 500mg tab",
-//         Units:"EA",
-//          ParLevel: 60,
-//          ReorderLevel: 30,
-//          ExpirationDate: new Date(),
-//          ExpirationDateFormatted: "10/03/2020",
-//          LocationDescription: "Carosel 01-01-01",
-//          QuantityOnHand: 55,
-//          ReorderSource: "Internal",
-//          ItmExpDateGranularity:"Month",
-//          QuantityMin:10,
-//          InStockQuantity:10,
-//          ItemDateFormat: "MM/DD/YYYY",
-//          PackageFormType: "B",
-//          PackageFormName: "Bulk",
-//          DrugId: "ACEtaminophen",
-//          ManufacturerName: "ARPON Labs",
-//       }));
-//       component.cycleCountItemsCopy.push(new GuidedManualCycleCountItemid({
-//         DeviceId: 5,
-//         DeviceDescription: 'carousel 2',
-//         DeviceLocationTypeId: '2023',
-//          ShelfNumber: 3,
-//          BinNumber: 2,
-//          SlotNumber: 1,
-//          DeviceLocationId: 86,  
-//          ItemId: "ace500t",
-//          BrandNameFormatted: "Tylenol 500mg tab",
-//          GenericNameFormatted: "acetaminophen 500mg tab",
-//         Units:"EA",
-//          ParLevel: 60,
-//          ReorderLevel: 30,
-//          ExpirationDate: new Date(),
-//          ExpirationDateFormatted: "10/03/2020",
-//          LocationDescription: "Carosel 01-01-01",
-//          QuantityOnHand: 55,
-//          ReorderSource: "Internal",
-//          ItmExpDateGranularity:"Month",
-//          QuantityMin:10,
-//          InStockQuantity:10,
-//          ItemDateFormat: "MM/DD/YYYY",
-//          PackageFormType: "B",
-//          PackageFormName: "Bulk",
-//          DrugId: "ACEtaminophen",
-//          ManufacturerName: "ARPON Labs",
-//       }));
-//       component.itemCount = 2;
-//       component.nextRecord();
-//       var bReturn = component.isLastItem;
-//       expect(bReturn).toBeFalsy();
-//     });
-//   });
-//   describe('next record with more than 1 records validation',()=>{
-//     it('returns the true if zero records',() => {
-//       component.currentItemCount = 1;
-//       component.cycleCountItemsCopy = [];
-//       component.cycleCountItemsCopy.push(new GuidedManualCycleCountItemid({
-//         DeviceId: 5,
-//         DeviceDescription: 'carousel 2',
-//         DeviceLocationTypeId: '2023',
-//          ShelfNumber: 3,
-//          BinNumber: 2,
-//          SlotNumber: 1,
-//          DeviceLocationId: 86,  
-//          ItemId: "ace500t",
-//          BrandNameFormatted: "Tylenol 500mg tab",
-//          GenericNameFormatted: "acetaminophen 500mg tab",
-//         Units:"EA",
-//          ParLevel: 60,
-//          ReorderLevel: 30,
-//          ExpirationDate: new Date(),
-//          ExpirationDateFormatted: "10/03/2020",
-//          LocationDescription: "Carosel 01-01-01",
-//          QuantityOnHand: 55,
-//          ReorderSource: "Internal",
-//          ItmExpDateGranularity:"Month",
-//          QuantityMin:10,
-//          InStockQuantity:10,
-//          ItemDateFormat: "MM/DD/YYYY",
-//          PackageFormType: "B",
-//          PackageFormName: "Bulk",
-//          DrugId: "ACEtaminophen",
-//          ManufacturerName: "ARPON Labs",
-//       }));
-//       component.cycleCountItemsCopy.push(new GuidedManualCycleCountItemid({
-//         DeviceId: 5,
-//         DeviceDescription: 'carousel 2',
-//         DeviceLocationTypeId: '2023',
-//          ShelfNumber: 3,
-//          BinNumber: 2,
-//          SlotNumber: 1,
-//          DeviceLocationId: 86,  
-//          ItemId: "ace500t",
-//          BrandNameFormatted: "Tylenol 500mg tab",
-//          GenericNameFormatted: "acetaminophen 500mg tab",
-//         Units:"EA",
-//          ParLevel: 60,
-//          ReorderLevel: 30,
-//          ExpirationDate: new Date(),
-//          ExpirationDateFormatted: "10/03/2020",
-//          LocationDescription: "Carosel 01-01-01",
-//          QuantityOnHand: 55,
-//          ReorderSource: "Internal",
-//          ItmExpDateGranularity:"Month",
-//          QuantityMin:10,
-//          InStockQuantity:10,
-//          ItemDateFormat: "MM/DD/YYYY",
-//          PackageFormType: "B",
-//          PackageFormName: "Bulk",
-//          DrugId: "ACEtaminophen",
-//          ManufacturerName: "ARPON Labs",
-//       }));
-//       component.itemCount = 2;
-//       component.currentItemCount = 1;
-//       component.nextRecord();
-//       var bReturn = component.isLastItem;
-//       expect(bReturn).toBeTruthy();
-//     });
-//   });
   describe('returns item expiredate granularity with month', () => {
     it('returns item expiredate granularity', () => {
       component.displayCycleCountItem = new GuidedManualCycleCountItemid({
@@ -534,17 +298,13 @@ describe('GuidedInvMgmtCycleCountPageComponent', () => {
   });
   describe('disable action buttons for last item', () => {
     it('disable action buttons', () => {
-      //component.isLastItem = true;
       component.DisableActionButtons(true); 
-      //expect(component.nextButtonDisable).toBeFalsy();
       expect(component.doneButtonDisable).toBeTruthy();
     });
   });
   describe('toggle action buttons when last item is false', () => {
     it('return with is last item', () => {
-      //component.isLastItem = false;
       component.DisableActionButtons(true); 
-      //expect(component.nextButtonDisable).toBeTruthy();
       expect(component.doneButtonDisable).toBeTruthy();
     });
   });
@@ -555,21 +315,10 @@ describe('GuidedInvMgmtCycleCountPageComponent', () => {
       component.numericElement = new NumericComponent(dummy,dummy1);
       component.numericElement.displayValue = "0";
       component.DisableActionButtons(true); 
-     // expect(component.nextButtonDisable).toBeTruthy();
       expect(component.doneButtonDisable).toBeTruthy();
     });
   });
-//   describe('null date changes validation', () => {
-//     it('date changes validation', () => {
-//      component.onDateChange(null);
-//       var dummy,dummy1;
-//        component.numericElement = new NumericComponent(dummy,dummy1);
-//        component.numericElement.displayValue = "0";
-//        component.DisableActionButtons(true); 
-//      // expect(component.nextButtonDisable).toBeTruthy();
-//       expect(component.doneButtonDisable).toBeTruthy;
-//     });
-//   });
+
   describe('future date changes validation', () => {
     it('date changes validation', () => {
       component.onDateChange("01/02/2021");
@@ -577,7 +326,6 @@ describe('GuidedInvMgmtCycleCountPageComponent', () => {
       component.numericElement = new NumericComponent(dummy,dummy1);
       component.numericElement.displayValue = "10";
       component.DisableActionButtons(false); 
-     // expect(component.nextButtonDisable).toBeFalsy();
       expect(component.doneButtonDisable).toBeFalsy();
     });
   });
@@ -680,57 +428,6 @@ describe('GuidedInvMgmtCycleCountPageComponent', () => {
     });
   });
 
-//   describe('navigateSkip', () => {
-//     describe('given last item', () => {
-//       beforeEach(() => {
-//         component.isLastItem = true;
-//       });
-
-//       it('calls ExecuteBackAction', () => {
-//         const item: any = {};
-//         component.displayCycleCountItem = item;
-//         const wpfActionControllerServiceStub: WpfActionControllerService = fixture.debugElement.injector.get(
-//           WpfActionControllerService
-//         );
-//         spyOn(
-//           wpfActionControllerServiceStub,
-//           'ExecuteBackAction'
-//         ).and.callThrough();
-//         component.isLastItem = false;
-//         component.navigateSkip();
-//         expect(
-//           wpfActionControllerServiceStub.ExecuteBackAction
-//         ).toHaveBeenCalled();
-//       });
-
-//       describe('and given carousel location', () => {
-//         let item: any = {};
-//         beforeEach(() => {
-//           item.DeviceLocationTypeId = DeviceLocationTypeId.Carousel;
-//           component.displayCycleCountItem = item;
-//         });
-
-//         it('should clear lightbar', () => {
-//           component.navigateSkip();
-//           expect(carouselLocationAccessService.clearLightbar).toHaveBeenCalled();
-//         })
-//       });
-
-//       describe('and given open storage location', () => {
-//         let item: any = {};
-//         beforeEach(() => {
-//           item.DeviceLocationTypeId = DeviceLocationTypeId.OpenStorage;
-//           component.displayCycleCountItem = item;
-//         });
-
-//         // it('should clear lightbar', () => {
-//         //   component.navigateSkip();
-//         //   expect(carouselLocationAccessService.clearLightbar).not.toHaveBeenCalled();
-//         // });
-//       });
-//     });
-//   });
-
   describe('toggle red border line for calendar control', () => {
     it('toggle red border line for calendar control', () => {
       var dummyElement = document.createElement('datepicker');
@@ -795,7 +492,6 @@ describe('GuidedInvMgmtCycleCountPageComponent', () => {
       component.DisableActionButtons(true); 
       var val = component.daterequired;
       expect(val).toBeTruthy();
-     // expect(component.nextButtonDisable).toBeTruthy();
       expect(component.doneButtonDisable).toBeTruthy();
     });
   });
@@ -808,7 +504,6 @@ describe('GuidedInvMgmtCycleCountPageComponent', () => {
       component.DisableActionButtons(true); 
       var val = component.daterequired;
       expect(val).toBeFalsy();
-     // expect(component.nextButtonDisable).toBeTruthy();
       expect(component.doneButtonDisable).toBeTruthy();
     });
   });
