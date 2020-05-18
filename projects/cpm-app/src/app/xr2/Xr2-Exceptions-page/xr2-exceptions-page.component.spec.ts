5e508c167f67f845cc8515f3cd5502f9e537083a
omnicell\rajui <rajui@omnicell.com>
2020-05-18 20:24:56 +0530

    Initial changes of angular pages

---
+++ projects/cpm-app/src/app/xr2/Xr2-Exceptions-page/xr2-exceptions-page.component.spec.ts
@@ -0,0 +1,36 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Xr2ExceptionsPageComponent } from './xr2-exceptions-page.component';
+import { MockTranslatePipe } from '../../core/testing/mock-translate-pipe.spec';
+import { GridModule, FooterModule, LayoutModule, SvgIconModule, SearchModule, ButtonActionModule } from '@omnicell/webcorecomponents';
+import { of } from 'rxjs';
+import { MockColHeaderSortable } from '../../shared/testing/mock-col-header-sortable.spec';
+import { MockAppHeaderContainer } from '../../core/testing/mock-app-header.spec';
+import { MockSearchPipe } from '../../core/testing/mock-search-pipe.spec';
+import { Xr2ExceptionsService } from '../../api-xr2/services/xr2-exceptions.service';
+
+describe('Xr2ExceptionsPageComponent', () => {
+  let component: Xr2ExceptionsPageComponent;
+  let fixture: ComponentFixture<Xr2ExceptionsPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Xr2ExceptionsPageComponent, MockTranslatePipe,
+        MockColHeaderSortable, MockAppHeaderContainer, MockSearchPipe ],
+      imports: [GridModule, FooterModule, LayoutModule, SearchModule, SvgIconModule, ButtonActionModule],
+      providers: [
+        { provide: Xr2ExceptionsService, useValue: { get: () => of([]) } },
+       ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Xr2ExceptionsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/cpm-app/src/app/xr2/Xr2-Exceptions-page/xr2-exceptions-page.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Xr2ExceptionsItem } from '../model/xr2-exceptions-item';
+import { Observable, of } from 'rxjs';
+import { map, switchMap, shareReplay } from 'rxjs/operators';
+import { SearchBoxComponent } from '@omnicell/webcorecomponents';
+import { IColHeaderSortChanged } from '../../shared/events/i-col-header-sort-changed';
+import * as _ from 'lodash';
+import { nameof } from '../../shared/functions/nameof';
+import { SortDirection } from '../../shared/constants/sort-direction';
+import { Many } from 'lodash';
+import { Xr2ExceptionsService } from '../../api-xr2/services/xr2-exceptions.service';
+
+@Component({
+  selector: 'app-xr2-exceptions-page',
+  templateUrl: './Xr2-Exceptions-page.component.html',
+  styleUrls: ['./Xr2-Exceptions-page.component.scss']
+})
+
+export class Xr2ExceptionsPageComponent implements OnInit, AfterViewInit {
+  readonly trayIDPropertyName = nameof<Xr2ExceptionsItem>("TrayID");
+  readonly trayDescriptionPropertyName = nameof<Xr2ExceptionsItem>("TrayDescription");
+  readonly exceptionPocketsPropertyName = nameof<Xr2ExceptionsItem>("ExceptionPockets");
+  readonly deviceNamePropertyName = nameof<Xr2ExceptionsItem>("DeviceName");
+  readonly completedDatePropertyName = nameof<Xr2ExceptionsItem>("CompletedDateTime");
+  @ViewChild('searchBox', null) searchElement: SearchBoxComponent;
+
+  displayExceptionsList$: Observable<Xr2ExceptionsItem[]>;
+  currentSortPropertyName: string = this.trayIDPropertyName;
+
+  searchTextFilter: string;
+  searchFields = [ this.trayIDPropertyName ];
+
+  constructor(
+    private exceptionsListService: Xr2ExceptionsService,
+
+    ) { }
+
+  ngOnInit() {
+    this.displayExceptionsList$ = this.exceptionsListService.get().pipe(map(guidedDeviceListItems => {
+       return this.sort(guidedDeviceListItems.map(p => new Xr2ExceptionsItem(p)), SortDirection.ascending);
+    }), shareReplay(1));
+  }
+
+
+  ngAfterViewInit() {
+    this.searchElement.searchOutput$
+      .pipe(
+        switchMap((searchData: string) => {
+          return of(searchData);
+        })
+      )
+      .subscribe(data => {
+        this.searchTextFilter = data;
+      });
+  }
+
+  columnSelected(event: IColHeaderSortChanged){
+    this.currentSortPropertyName = event.ColumnPropertyName;
+    this.displayExceptionsList$ = this.displayExceptionsList$.pipe(map(exceptions => {
+      return this.sort(exceptions, event.SortDirection);
+    }));
+  }
+
+  sort(devices: Xr2ExceptionsItem[], sortDirection: Many<boolean|"asc"|"desc">): Xr2ExceptionsItem[]{
+      return _.orderBy(devices, x => x[this.currentSortPropertyName], sortDirection);
+  }
+}
