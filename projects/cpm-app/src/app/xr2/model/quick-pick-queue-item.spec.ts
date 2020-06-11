4f2dc96f9245d00aeb22db1d921e815cce8f2c10
omnicell\josephr <joseph.ross@omnicell.com>
2020-06-11 18:35:15 -0400

    Fixed test

--- projects/cpm-app/src/app/xr2/model/quick-pick-queue-item.spec.ts
+++ projects/cpm-app/src/app/xr2/model/quick-pick-queue-item.spec.ts
@@ -2,6 +2,6 @@ import { QuickPickQueueItem } from './quick-pick-queue-item';
 
 describe('QuickPickQueueItem', () => {
   it('should create an instance', () => {
-    expect(new QuickPickQueueItem()).toBeTruthy();
+    expect(new QuickPickQueueItem(null)).toBeTruthy();
   });
 });
