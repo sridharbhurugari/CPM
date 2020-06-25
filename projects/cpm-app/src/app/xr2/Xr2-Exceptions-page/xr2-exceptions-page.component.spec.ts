729bf77139e6fe9d3061818f24d91a5386cbcf19
omnicell\rajui <rajui@omnicell.com>
2020-06-25 17:14:40 +0530

    Scan to open exception trays angular changes

--- projects/cpm-app/src/app/xr2/Xr2-Exceptions-page/xr2-exceptions-page.component.spec.ts
+++ projects/cpm-app/src/app/xr2/Xr2-Exceptions-page/xr2-exceptions-page.component.spec.ts
@@ -1,21 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Xr2ExceptionsPageComponent } from './xr2-exceptions-page.component';
 import { MockTranslatePipe } from '../../core/testing/mock-translate-pipe.spec';
-import { GridModule, FooterModule, LayoutModule, SvgIconModule, SearchModule, ButtonActionModule } from '@omnicell/webcorecomponents';
-import { of, never } from 'rxjs';
+import { GridModule, FooterModule, LayoutModule, SvgIconModule, SearchModule, ButtonActionModule, PopupDialogService } from '@omnicell/webcorecomponents';
+import { of, never, Subject } from 'rxjs';
 import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
 import { MockColHeaderSortable } from '../../shared/testing/mock-col-header-sortable.spec';
 import { MockAppHeaderContainer } from '../../core/testing/mock-app-header.spec';
 import { MockSearchPipe } from '../../core/testing/mock-search-pipe.spec';
 import { Xr2ExceptionsService } from '../../api-xr2/services/xr2-exceptions.service';
 import { IColHeaderSortChanged } from '../../shared/events/i-col-header-sort-changed';
+import { TranslateService } from '@ngx-translate/core';
 import { Xr2ExceptionsItem } from '../model/xr2-exceptions-item';
 import { ActivatedRoute, Router, Params, NavigationExtras, RouterModule } from '@angular/router';
 //import { ColHeaderSortableComponent } from '../../shared/components/col-header-sortable/col-header-sortable.component';
-import { map } from 'rxjs/operators';
+import { map, switchMap, shareReplay } from 'rxjs/operators';
 import {Location} from '@angular/common';
 import { ColHeaderSortableComponent } from '../../shared/components/col-header-sortable/col-header-sortable.component';
 import { IXr2ExceptionsItem } from '../../api-xr2/data-contracts/i-xr2-exception-item';
+import { SystemConfigurationService } from '../../shared/services/system-configuration.service';
+import { BarcodeScanService } from 'oal-core';
+import { IConfigurationValue } from '../../shared/interfaces/i-configuration-value';
 
 describe('Xr2ExceptionsPageComponent', () => {
   let component: Xr2ExceptionsPageComponent;
@@ -23,27 +27,50 @@ describe('Xr2ExceptionsPageComponent', () => {
   let event: IColHeaderSortChanged = {ColumnPropertyName:"TrayID",SortDirection:"asc"};
   let eventSelected: IXr2ExceptionsItem = {TrayID:"c00004", DeviceID:"5",CompletedDateTime:"2020-06-01 07:41:19.763",TrayDescription:"",ExceptionPockets:"",DeviceName:""};
   let router: Partial<Router>;
+  let xr2ExceptionsService: Partial<Xr2ExceptionsService>;
   let wpfActionControllerService: Partial<WpfActionControllerService>;
+  let systemConfigurationService: Partial<SystemConfigurationService>;
+  let translateService: Partial<TranslateService>;
+  let popupDialogService: Partial<PopupDialogService>;
+  let barcodeScanService: Partial<BarcodeScanService>;
+  let configurationValue: IConfigurationValue = { Value: '15', Category: '', SubCategory: '' };
   beforeEach(async(() => {
+    xr2ExceptionsService = {  get: () => of([]), 
+    gettraytypes: () => of([])};
     wpfActionControllerService = { ExecuteWpfContinueNavigationAction: jasmine.createSpy('ExecuteWpfContinueNavigationAction'),
       ExecuteContinueNavigationAction: jasmine.createSpy('ExecuteContinueNavigationAction')};
+
+      //systemConfigurationService = { GetConfigurationValues: jasmine.createSpy('GetConfigurationValues')};
+      systemConfigurationService = { GetConfigurationValues: () => of(configurationValue) };
+      translateService = { get: jasmine.createSpy('get')};
+      barcodeScanService = { 
+        reset: jasmine.createSpy('reset').and.returnValue(of({})),
+        handleKeyInput: jasmine.createSpy('handleKeyInput').and.returnValue(of({})),
+        isScannerInput: jasmine.createSpy('isScannerInput').and.returnValue(of({})),
+      };
 
     TestBed.configureTestingModule({
       declarations: [ Xr2ExceptionsPageComponent, MockTranslatePipe,
         MockColHeaderSortable, MockAppHeaderContainer, MockSearchPipe],
       imports: [GridModule, FooterModule, LayoutModule, SearchModule,  ButtonActionModule],
       providers: [
-        { provide: Xr2ExceptionsService, useValue: { get: () => of([]) } },
+        { provide: Xr2ExceptionsService, useValue: xr2ExceptionsService},
         { provide: WpfActionControllerService, useValue: wpfActionControllerService },
+        { provide: SystemConfigurationService, useValue: systemConfigurationService },
+        { provide: TranslateService, useValue: translateService },
+        { provide: PopupDialogService, useValue: popupDialogService },
+        { provide: BarcodeScanService, useValue: barcodeScanService },
        ]
     })
     .compileComponents();
+
 
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(Xr2ExceptionsPageComponent);
     component = fixture.componentInstance;
+
     //component.sortOrder = "desc";
     //event.ColumnPropertyName = "TrayID";
     //event.SortDirection="asc";
@@ -63,9 +90,15 @@ describe('Xr2ExceptionsPageComponent', () => {
     expect(component.columnSelected(event));
   });
   describe('navigation on page', () => {
-    it('navigates to manual cycle count page', () => {
+    it('navigates details page', () => {
        component.navigatedetailspage(eventSelected);
        //expect(wpfActionControllerService.ExecuteWpfContinueNavigationAction).toHaveBeenCalled();
     });
   });
+  // describe('hook up event handler calling', () => {
+  //   it('should invoke the method of hook up event handler', () => {
+  //      spyOn(barcodeScanService,)
+  //      //expect(wpfActionControllerService.ExecuteWpfContinueNavigationAction).toHaveBeenCalled();
+  //   });
+  // });
 });
--- projects/cpm-app/src/app/xr2/Xr2-Exceptions-page/xr2-exceptions-page.component.ts
+++ projects/cpm-app/src/app/xr2/Xr2-Exceptions-page/xr2-exceptions-page.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit,  ViewChild, Output, EventEmitter,AfterViewInit} from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, AfterViewInit, HostListener } from '@angular/core';
 import { Xr2ExceptionsItem } from '../model/xr2-exceptions-item';
-import { Observable, of,Subscription } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 import { map, switchMap, shareReplay } from 'rxjs/operators';
-import { SearchBoxComponent } from '@omnicell/webcorecomponents';
+import { SearchBoxComponent, PopupDialogService, PopupDialogComponent, PopupDialogProperties, PopupDialogType } from '@omnicell/webcorecomponents';
 import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
 import { IColHeaderSortChanged } from '../../shared/events/i-col-header-sort-changed';
 import * as _ from 'lodash';
 import { nameof } from '../../shared/functions/nameof';
 import { SortDirection } from '../../shared/constants/sort-direction';
+import { TranslateService } from '@ngx-translate/core';
 import { Many } from 'lodash';
+import { SystemConfigurationService } from '../../shared/services/system-configuration.service';
 import { Xr2ExceptionsService } from '../../api-xr2/services/xr2-exceptions.service';
 import { ColHeaderSortableComponent } from '../../shared/components/col-header-sortable/col-header-sortable.component';
 import { IXr2ExceptionsItem } from '../../api-xr2/data-contracts/i-xr2-exception-item';
+import { BarcodeScanService } from 'oal-core';
 @Component({
   selector: 'app-xr2-exceptions-page',
   templateUrl: './xr2-exceptions-page.component.html',
@@ -25,30 +28,56 @@ export class Xr2ExceptionsPageComponent implements OnInit, AfterViewInit {
   readonly deviceNamePropertyName = nameof<Xr2ExceptionsItem>("DeviceName");
   readonly completedDatePropertyName = nameof<Xr2ExceptionsItem>("CompletedDateTime");
   searchSub: Subscription;
+  pagelevelInput: string;
   @ViewChild('searchBox', null) searchElement: SearchBoxComponent;
 
   displayExceptionsList$: Observable<Xr2ExceptionsItem[]>;
+  traytypesList$: Observable<string[]>;
+  traytypesList: string[];
   currentSortPropertyName: string = this.completedDatePropertyName;
   sortOrder: SortDirection = SortDirection.descending;
   searchTextFilter: string;
-  searchFields = [ this.trayIDPropertyName,this.exceptionPocketsPropertyName,this.trayTypePropertyName, this.deviceNamePropertyName];
-
+  nonBarCodekeyboardInput: string = '';
+  nonBarcodeInputFocus: boolean = false;
+  rawBarcodeMessage: string = '';
+  popupTimeoutSeconds: number;
+  searchFields = [this.trayIDPropertyName, this.exceptionPocketsPropertyName, this.trayTypePropertyName, this.deviceNamePropertyName];
+  private barcodeScannedSubscription: Subscription;
   constructor(
+    private translateService: TranslateService,
+    private systemConfigurationService: SystemConfigurationService,
     private exceptionsListService: Xr2ExceptionsService,
+    private dialogService: PopupDialogService,
     private wpfActionControllerService: WpfActionControllerService,
+    private _barcodeScanService: BarcodeScanService
 
-    ) { }
+  ) { }
 
   ngOnInit() {
-    this.displayExceptionsList$ = this.exceptionsListService.get().pipe(map(guidedDeviceListItems => {
-       return this.sort(guidedDeviceListItems.map(p => new Xr2ExceptionsItem(p)), SortDirection.descending);
+
+    this.traytypesList$ = this.exceptionsListService.gettraytypes().pipe(map(traytypelistitems => {
+      return traytypelistitems.map<string>(p => p.toString());
     }), shareReplay(1));
 
-    
+    this.displayExceptionsList$ = this.exceptionsListService.get().pipe(map(guidedDeviceListItems => {
+      return this.sort(guidedDeviceListItems.map(p => new Xr2ExceptionsItem(p)), SortDirection.descending);
+    }), shareReplay(1));
+
+    this.systemConfigurationService.GetConfigurationValues('TIMEOUTS', 'POP_UP_MESSAGE_TIMEOUT').subscribe(result => {
+      console.log('popup message timeout : ' + result);
+      this.popupTimeoutSeconds = (Number(result.Value));
+    });
+
+    this.traytypesList$ && this.traytypesList$.subscribe(x => {
+      this.traytypesList = x;
+    });
+
+    this.hookupEventHandlers();
   }
 
 
   ngAfterViewInit() {
+
     this.searchSub = this.searchElement.searchOutput$
       .pipe(
         switchMap((searchData: string) => {
@@ -60,7 +89,7 @@ export class Xr2ExceptionsPageComponent implements OnInit, AfterViewInit {
       });
   }
 
-  columnSelected(event: IColHeaderSortChanged){
+  columnSelected(event: IColHeaderSortChanged) {
     this.currentSortPropertyName = event.ColumnPropertyName;
     this.sortOrder = event.SortDirection;
     this.displayExceptionsList$ = this.displayExceptionsList$.pipe(map(exceptions => {
@@ -68,16 +97,179 @@ export class Xr2ExceptionsPageComponent implements OnInit, AfterViewInit {
     }));
   }
 
-  sort(devices: Xr2ExceptionsItem[], sortDirection: Many<boolean|"desc"|"asc">): Xr2ExceptionsItem[]{
-      return _.orderBy(devices, x => x[this.currentSortPropertyName], sortDirection);
+  sort(devices: Xr2ExceptionsItem[], sortDirection: Many<boolean | "desc" | "asc">): Xr2ExceptionsItem[] {
+    return _.orderBy(devices, x => x[this.currentSortPropertyName], sortDirection);
   }
 
   ngOnDestroy(): void {
     if (this.searchSub) { this.searchSub.unsubscribe(); }
+    if (this.barcodeScannedSubscription) { this.barcodeScannedSubscription.unsubscribe(); }
   }
 
-  navigatedetailspage(exceptions: IXr2ExceptionsItem)
-  {
-    this.wpfActionControllerService.ExecuteContinueNavigationAction(`stocking/exceptiondetails`,{TrayID:exceptions.TrayID.toString(),DeviceID: exceptions.DeviceID,CompletedDateTime:exceptions.CompletedDateTime, DeviceName:exceptions.DeviceName, TrayDescription: exceptions.TrayDescription});
+  onBarcodeScanExcludedKeyPressEvent(event: KeyboardEvent) {
+    var isInputComplete = this._barcodeScanService.handleKeyInput(event);
+    var isScannerInput = this._barcodeScanService.isScannerInput();
+    // check if the character is a barcode scan
+    if (isScannerInput) {
+      //Since the first character always returns true, ignore it.
+      if (this._barcodeScanService.BarcodeInputCharacters.length != 1) {
+        //ignore if it is a barcodescan
+        event.preventDefault();
+      }
+    } else {
+      this._barcodeScanService.reset();
+    }
+    if (isInputComplete) {
+      //remove the last character.
+      this.nonBarCodekeyboardInput = this.nonBarCodekeyboardInput.slice(0, -1);
+      this.rawBarcodeMessage = this._barcodeScanService.BarcodeInputCharacters;
+      console.log(`Barcode Scan from NonBarcode Enabled Text box:  ${this._barcodeScanService.BarcodeInputCharacters}`);
+      this._barcodeScanService.reset();
+      var value = this.traytypesList$;
+      var exceptionData: IXr2ExceptionsItem;
+      this.traytypesList.forEach(function (value) {
+        if (value.toString().toUpperCase() === this.rawBarcodeMessage.toString().substring(1, 2).toUpperCase()) {
+          this.displayExceptionsList$.subscribe((exceptions: IXr2ExceptionsItem[]) =>
+            exceptionData = exceptions.find(p => p.TrayID.toString().toUpperCase() === this.rawBarcodeMessage.toString().toUpperCase())
+          );
+          if (exceptionData)
+            this.navigatedetailspage(exceptionData);
+          else
+            this.displayWrongBarCodeDialog(true);
+        }
+        else {
+          this.displayWrongBarCodeDialog(false);
+        }
+      });
+      if (this.traytypesList$.forEach(
+        val => val.toString().toUpperCase() === this.rawBarcodeMessage.toString().substring(1, 2).toUpperCase())) {
+        this.displayExceptionsList$.subscribe((exceptions: IXr2ExceptionsItem[]) =>
+          exceptionData = exceptions.find(p => p.TrayID.toString().toUpperCase() === this.rawBarcodeMessage.toString().toUpperCase())
+        );
+        if (exceptionData)
+          this.navigatedetailspage(exceptionData);
+        else
+          this.displayWrongBarCodeDialog(true);
+      }
+      else {
+        this.displayWrongBarCodeDialog(false);
+      }
+    }
+  }
+
+  @HostListener("document:keypress", ['$event']) onKeypressHandler(event: KeyboardEvent) {
+    console.log(event);
+    if (!this.nonBarcodeInputFocus) {
+      var isInputComplete = this._barcodeScanService.handleKeyInput(event);
+      //If not from barcode scanner ignore the character
+      if (!this._barcodeScanService.isScannerInput()) {
+        this._barcodeScanService.reset();
+      }
+      if (isInputComplete) {
+        //populating the page level input into text box.
+        this.pagelevelInput = this._barcodeScanService.BarcodeInputCharacters;
+        this.rawBarcodeMessage = this._barcodeScanService.BarcodeInputCharacters;
+        this._barcodeScanService.reset();
+        var value = this.traytypesList$;
+        var exceptionData: IXr2ExceptionsItem;
+        if (this.traytypesList$.forEach(
+          val => val.toString().toUpperCase() === this.rawBarcodeMessage.toString().substring(1, 2).toUpperCase())) {
+          this.displayExceptionsList$.subscribe((exceptions: IXr2ExceptionsItem[]) =>
+            exceptionData = exceptions.find(p => p.TrayID.toString().toUpperCase() === this.rawBarcodeMessage.toString().toUpperCase())
+          );
+          if (exceptionData)
+            this.navigatedetailspage(exceptionData);
+          else
+            this.displayWrongBarCodeDialog(true);
+        }
+        else {
+          this.displayWrongBarCodeDialog(false);
+        }
+      }
+    }
+  }
+  reset() {
+    this.rawBarcodeMessage = '';
+  }
+
+  private hookupEventHandlers(): void {
+    if (this.isInvalid(this._barcodeScanService)) {
+      return;
+    }
+    this.barcodeScannedSubscription = this._barcodeScanService.BarcodeScannedSubject && this._barcodeScanService.BarcodeScannedSubject.subscribe((scannedBarcode: string) =>
+      this.processScannedBarcode(scannedBarcode)
+    );
+  }
+
+  private unhookEventHandlers(): void {
+    if (this.isInvalid(this._barcodeScanService)) {
+      return;
+    }
+
+    this.unsubscribeIfValidSubscription(this.barcodeScannedSubscription);
+  }
+
+  private unsubscribeIfValidSubscription(subscription: Subscription): void {
+    if (this.isValid(subscription)) {
+      subscription.unsubscribe();
+    }
+  }
+
+  private isValid(variable: any): boolean {
+    return variable !== undefined && variable !== null;
+  }
+
+  private isInvalid(variable: any): boolean {
+    return !this.isValid(variable);
+  }
+  private processScannedBarcode(scannedBarcode: string): void {
+    this._barcodeScanService.reset();
+    this.rawBarcodeMessage = scannedBarcode;
+    var exceptionData: IXr2ExceptionsItem;
+    var traytypeFound = false;
+    for (var i = 0; i < this.traytypesList.length; i++) {
+      var value = this.traytypesList[i];
+      if (value.toString().toUpperCase() === this.rawBarcodeMessage.toString().substring(0, 2).toUpperCase()) {
+        traytypeFound = true;
+        break;
+      }
+      else {
+        traytypeFound = false;
+      }
+    }
+
+    if (traytypeFound) {
+      this.displayExceptionsList$.subscribe((exceptions: IXr2ExceptionsItem[]) =>
+        exceptionData = exceptions.find(p => p.TrayID.toString().toUpperCase() === this.rawBarcodeMessage.toString().toUpperCase())
+      );
+      if (exceptionData)
+        this.navigatedetailspage(exceptionData);
+      else
+        this.displayWrongBarCodeDialog(true);
+    }
+    else {
+      this.displayWrongBarCodeDialog(false);
+    }
+  }
+  navigatedetailspage(exceptions: IXr2ExceptionsItem) {
+    this.wpfActionControllerService.ExecuteContinueNavigationAction(`stocking/exceptiondetails`, { TrayID: exceptions.TrayID.toString(), DeviceID: exceptions.DeviceID, CompletedDateTime: exceptions.CompletedDateTime, DeviceName: exceptions.DeviceName, TrayDescription: exceptions.TrayDescription });
+  }
+  displayWrongBarCodeDialog(trays: boolean): void {
+    const properties = new PopupDialogProperties('INVALID_TRAY_SCAN_DESC');
+    this.translateService.get('INVALID_TRAY_SCAN').subscribe(result => { properties.titleElementText = result; });
+    if (trays) {
+      this.translateService.get('INVALID_TRAY_SCAN').subscribe(result => { properties.titleElementText = result; });
+      this.translateService.get('INVALID_NOEXCEPTIONS_SCAN_DESC').subscribe(result => { properties.messageElementText = result; });
+    }
+    else {
+      this.translateService.get('INVALID_SCAN_BARCODE_HEADER').subscribe(result => { properties.titleElementText = result; });
+      this.translateService.get('INVALID_TRAY_SCAN_DESC').subscribe(result => { properties.messageElementText = result; });
+
+    } properties.showPrimaryButton = true;
+    properties.showSecondaryButton = false;
+    properties.primaryButtonText = 'OK';
+    properties.dialogDisplayType = PopupDialogType.Warning;
+    properties.timeoutLength = this.popupTimeoutSeconds;
+    this.dialogService.showOnce(properties);
   }
 }
