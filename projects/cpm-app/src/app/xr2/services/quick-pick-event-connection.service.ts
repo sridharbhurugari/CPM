6c92b33a3d11304729ebeb26ee4170eb631affaa
omnicell\rajui <rajui@omnicell.com>
2020-07-24 02:37:21 +0530

    Merge conflicts resolved

--- projects/cpm-app/src/app/xr2/services/quick-pick-event-connection.service.ts
+++ projects/cpm-app/src/app/xr2/services/quick-pick-event-connection.service.ts
@@ -11,6 +11,7 @@ export class QuickPickEventConnectionService {
   public QuickPickReloadDrawersSubject = new Subject<QuickPickDrawerData>();
   public QuickPickDrawerUpdateSubject = new Subject<QuickPickDrawerData>();
   public QuickPickQueueUpdateSubject = new Subject<any>();
+  public QuickPickErrorUpdateSubject = new Subject<any>();
 
   constructor(
       private eventConnectionService: EventConnectionService
@@ -42,6 +43,11 @@ export class QuickPickEventConnectionService {
     if (message.EventId === 'QuickPickQueueUpdateEvent') {
       console.log(message);
       this.QuickPickQueueUpdateSubject.next(message);
+      return;
+    }
+    if (message.EventId === 'QuickPickErrorUpdateEvent') {
+      console.log(message);
+      this.QuickPickErrorUpdateSubject.next(message);
       return;
     }
   }
