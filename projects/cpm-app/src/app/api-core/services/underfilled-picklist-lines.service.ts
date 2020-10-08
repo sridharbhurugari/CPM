825f4204d1a11d2702cf8adc74c85a0c517bee95
warren <warren.junker@omnicell.com>
2020-10-08 09:57:25 -0400

    calls

--- projects/cpm-app/src/app/api-core/services/underfilled-picklist-lines.service.ts
+++ projects/cpm-app/src/app/api-core/services/underfilled-picklist-lines.service.ts
@@ -21,4 +21,18 @@ export class UnderfilledPicklistLinesService {
       headers: this.ocapHttpHeadersService.getHeaders()
     });
   }
+
+  /** ClosePicklistLines(List<Guid> picklistLineIds) */
+  public close(picklistLineIds: string[]) {
+    const url = this.ocapUrlBuilderService.buildUrl(`/api/picklists/underfilled/picklistLines/close/${picklistLineIds}`);
+    const serviceHeaders = this.ocapHttpHeadersService.getHeaders();
+    return this.httpClient.post(url, { headers: serviceHeaders });
+  }
+
+   public reset(picklistLineIds: string[]) {
+     const url = this.ocapUrlBuilderService.buildUrl(`api/pickRoutes/Reset/${picklistLineIds}`);
+     const serviceHeaders = this.ocapHttpHeadersService.getHeaders();
+     return this.httpClient.post(url, { headers: serviceHeaders });
+    }
+
 }
