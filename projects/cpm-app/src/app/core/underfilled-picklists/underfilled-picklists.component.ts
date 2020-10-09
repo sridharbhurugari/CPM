0a745ba066350088b13177ff71388034114b5d78
dave molesky <dmolesky04@gmail.com>
2020-10-09 08:14:12 -0400

    Display prompt to delete box. Disable buttons when saving

--- projects/cpm-app/src/app/core/underfilled-picklists/underfilled-picklists.component.ts
+++ projects/cpm-app/src/app/core/underfilled-picklists/underfilled-picklists.component.ts
@@ -3,13 +3,16 @@ import { UnderfilledPicklist } from '../model/underfilled-picklist';
 import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
 import { UnderfilledPicklistsService } from '../../api-core/services/underfilled-picklists.service';
 import { WindowService } from '../../shared/services/window-service';
-import { SearchBoxComponent } from '@omnicell/webcorecomponents';
+import { SearchBoxComponent, PopupDialogProperties, PopupDialogType, PopupDialogService, PopupWindowProperties, PopupWindowService } from '@omnicell/webcorecomponents';
 import { nameof } from '../../shared/functions/nameof';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, take } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { IColHeaderSortChanged } from '../../shared/events/i-col-header-sort-changed';
 import * as _ from 'lodash';
 import { SortDirection } from '../../shared/constants/sort-direction';
+import { TranslateService } from '@ngx-translate/core';
+import { IConfirmPopupData } from '../../shared/model/i-confirm-popup-data';
+import { ConfirmPopupComponent } from '../../shared/components/confirm-popup/confirm-popup.component';
 
 @Component({
   selector: 'app-underfilled-picklists',
@@ -40,6 +43,7 @@ export class UnderfilledPicklistsComponent implements AfterViewInit{
   descriptionPropertyName =  nameof<UnderfilledPicklist>('DescriptionSearchValue');
   destinationPropertyName = nameof<UnderfilledPicklist>('DesintationSearchValue');
   datePropertyName = nameof<UnderfilledPicklist>('CompletedDate');
+  popupTimeoutSeconds = 10;
 
   currentSortPropertyName : string = this.datePropertyName;
   sortOrder: SortDirection = SortDirection.descending;
@@ -59,7 +63,8 @@ export class UnderfilledPicklistsComponent implements AfterViewInit{
   constructor(
     private windowService: WindowService,
     private wpfActionControllerService: WpfActionControllerService,
-    private underfilledPicklistsService: UnderfilledPicklistsService
+    private underfilledPicklistsService: UnderfilledPicklistsService,
+    private popupWindowService: PopupWindowService
   ) {
   }
 
@@ -88,10 +93,41 @@ export class UnderfilledPicklistsComponent implements AfterViewInit{
   }
 
   delete(underfilledPicklist: UnderfilledPicklist) {
+    this.displayDeletePromptDialog(underfilledPicklist);
+  }
+
+  executeDelete(underfilledPicklist: UnderfilledPicklist) {
+    underfilledPicklist.Saving = true;
     this.underfilledPicklistsService.delete(underfilledPicklist.OrderId).subscribe(x => {
-      alert('success');
+      underfilledPicklist.Saving = false;
+      _.remove(this.picklists, {
+        OrderId: underfilledPicklist.OrderId
+      });
     }, y => {
+      underfilledPicklist.Saving = false;
       alert('failure');
     });
   }
+
+  view(underfilledPicklist: UnderfilledPicklist) {
+    this.navigate(underfilledPicklist.OrderId);
+  }
+
+  displayDeletePromptDialog(underfilledPicklist: UnderfilledPicklist) {
+    const properties = new PopupWindowProperties();
+    const data: IConfirmPopupData = {
+      headerResourceKey: 'UNFILLED_DELET_PICKLIST_PROMPT_TITLE',
+      confirmTextboxResourceKey: 'UNFILLED_DELET_PICKLIST_PROMPT_MESSAGE'
+    };
+
+    properties.data = data;
+
+    let component = this.popupWindowService.show(ConfirmPopupComponent, properties) as unknown as ConfirmPopupComponent;
+    component.dismiss.pipe(take(1)).subscribe(selectedConfirm => {
+      if (selectedConfirm) {
+        this.executeDelete(underfilledPicklist)
+      }
+    });
+  }
+
 }
