7a8454d9c000b2a546f48c47c1874542e29c72e2
Milan Fatschel <milan.fatschel@omnicell.com>
2020-10-13 11:59:03 -0400

    clean

--- projects/cpm-app/src/app/shared/constants/destination-types.ts
+++ projects/cpm-app/src/app/shared/constants/destination-types.ts
@@ -1,5 +1,5 @@
 export class DestinationTypes {
   public static readonly Area: string = 'A';
   public static readonly Patient: string = 'P';
-  public static readonly Cabinet: string = 'O';
+  public static readonly Omni: string = 'O';
 }
