94a71c8ad932ffbcd9a5b34c06b9aa2076ef9754
dave molesky <dmolesky04@gmail.com>
2020-10-13 07:34:56 -0400

    Update services to match apis

--- projects/cpm-app/src/app/api-core/services/underfilled-picklist-lines.service.ts
+++ projects/cpm-app/src/app/api-core/services/underfilled-picklist-lines.service.ts
@@ -22,17 +22,13 @@ export class UnderfilledPicklistLinesService {
     });
   }
 
-  /** ClosePicklistLines(List<Guid> picklistLineIds) */
   public close(picklistLineIds: string[]) {
-    const url = this.ocapUrlBuilderService.buildUrl(`/api/picklists/underfilled/picklistLines/close/${picklistLineIds}`);
+    const url = this.ocapUrlBuilderService.buildUrl(`/api/picklists/underfilled/picklistLines/close`);
     const serviceHeaders = this.ocapHttpHeadersService.getHeaders();
-    return this.httpClient.post(url, { headers: serviceHeaders });
+    const data = {
+      PicklistLineIds: picklistLineIds
+    };
+    return this.httpClient.post(url, data, { headers: serviceHeaders });
   }
-
-   public reset(picklistLineIds: string) {
-     const url = this.ocapUrlBuilderService.buildUrl(`api/pickRoutes/Reset/${picklistLineIds}`);
-     const serviceHeaders = this.ocapHttpHeadersService.getHeaders();
-     return this.httpClient.post(url, { headers: serviceHeaders });
-    }
 
 }
