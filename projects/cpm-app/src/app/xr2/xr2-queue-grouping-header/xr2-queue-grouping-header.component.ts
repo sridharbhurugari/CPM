0a5507719aa0eed4774ad1a274f7c2751222b881
Geetha Karunala <GeethaK@omnicell.com>
2020-10-15 15:45:05 -0400

    595616 - XR2 Queue Grouping Screen Xr2 selector

--- projects/cpm-app/src/app/xr2/xr2-queue-grouping-header/xr2-queue-grouping-header.component.ts
+++ projects/cpm-app/src/app/xr2/xr2-queue-grouping-header/xr2-queue-grouping-header.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, AfterViewInit, ViewChild, Output, EventEmitter } from '@angular/core';
-import { SearchBoxComponent } from '@omnicell/webcorecomponents';
+import { SearchBoxComponent, SingleselectRowItem } from '@omnicell/webcorecomponents';
 import { of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { WindowService } from '../../shared/services/window-service';
+import { SelectableDeviceInfo } from "../../shared/model/selectable-device-info";
+import { OcapHttpConfigurationService } from "../../shared/services/ocap-http-configuration.service";
+import { Xr2QuickPickQueueDeviceService } from "../../api-xr2/services/xr2-quick-pick-queue-device.service";
 
 @Component({
   selector: 'app-xr2-queue-grouping-header',
@@ -14,16 +17,86 @@ export class Xr2QueueGroupingHeaderComponent implements OnInit, AfterViewInit {
 
   @Output() searchTextFilterEvent: EventEmitter<string> = new EventEmitter<string>();
 
+  selectedDeviceInformation: SelectableDeviceInfo;
+  deviceInformationList: SelectableDeviceInfo[];
+  outputDeviceDisplayList: SingleselectRowItem[] = [];
+  defaultDeviceDisplyItem: SingleselectRowItem;
+
   @ViewChild('searchBox', {
     static: true
   })
 
   searchElement: SearchBoxComponent;
 
-  constructor(private windowService: WindowService) { }
+  constructor(private windowService: WindowService,
+              private ocapHttpConfigurationService: OcapHttpConfigurationService,
+              private quickPickDeviceService: Xr2QuickPickQueueDeviceService) { }
 
   ngOnInit() {
+    this.getXr2Devices();
   }
+
+  async getXr2Devices() {
+    this.deviceInformationList = await this.quickPickDeviceService.get().toPromise();
+    const newList: SingleselectRowItem[] = [];
+
+    const currentClientId = this.ocapHttpConfigurationService.get().clientId;
+    let defaultFound: SingleselectRowItem;
+
+    if (this.deviceInformationList.length === 1) {
+      defaultFound = new SingleselectRowItem(
+        this.deviceInformationList[0].Description,
+        this.deviceInformationList[0].DeviceId.toString()
+      );
+      newList.push(defaultFound);
+    } else {
+      const selectAll = new SingleselectRowItem("All", "All", true);
+      newList.push(selectAll);
+      this.deviceInformationList.forEach((selectableDeviceInfo) => {
+        const selectRow = new SingleselectRowItem(
+          selectableDeviceInfo.Description,
+          selectableDeviceInfo.DeviceId.toString(),
+          selectableDeviceInfo.IsActive
+        );
+        
+        newList.push(selectRow);
+
+        if (
+          !defaultFound &&
+          selectableDeviceInfo.CurrentLeaseHolder.toString() === currentClientId
+        ) {
+          defaultFound = selectRow;
+        }
+      });
+    }
+    
+    this.outputDeviceDisplayList = newList;
+
+    if (defaultFound) {
+      this.defaultDeviceDisplyItem = this.outputDeviceDisplayList.find(
+        (x) => x.value === defaultFound.value
+      );
+      this.loadSelectedDeviceInformation(defaultFound.value);
+    } else {
+      this.defaultDeviceDisplyItem = this.outputDeviceDisplayList.find(
+        (x) => x.value === "All"
+       );
+    }
+  }
+
+  private loadSelectedDeviceInformation(deviceId: string) {
+    const indexToLoad = this.deviceInformationList.findIndex(
+      (deviceInformation) => {
+        return deviceInformation.DeviceId.toString() === deviceId;
+      }
+    );
+
+    if (indexToLoad !== -1) {
+      this.selectedDeviceInformation = this.deviceInformationList[indexToLoad];
+    }
+  }
+ 
+
 
   ngAfterViewInit() {
     this.searchElement.searchOutput$
