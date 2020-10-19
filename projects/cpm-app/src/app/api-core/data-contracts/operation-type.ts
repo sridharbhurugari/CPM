49af2c3d0ecb8307930a2a323907338a89f649f8
dave molesky <dmolesky04@gmail.com>
2020-10-19 09:46:45 -0400

    Add workstation tracker service

---
+++ projects/cpm-app/src/app/api-core/data-contracts/operation-type.ts
@@ -0,0 +1,6 @@
+export enum OperationType {
+  Pick = 0,
+  PickDelete = 1,
+  Stock = 2,
+  Receive = 3
+}
--- projects/cpm-app/src/app/api-core/services/workstation-tracker.service.ts
+++ projects/cpm-app/src/app/api-core/services/workstation-tracker.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { WorkstationTrackerData } from '../data-contracts/workstation-tracker-data';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { OcapHttpHeadersService } from '../../shared/services/ocap-http-headers.service';
+import { OcapUrlBuilderService } from '../../shared/services/ocap-url-builder.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkstationTrackerService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient,
+    private ocapUrlBuilderService: OcapUrlBuilderService,
+    private ocapHttpHeadersService: OcapHttpHeadersService
+    ) { }
+
+    public GetWorkstationShortNames(workstationTrackerData: WorkstationTrackerData): Observable<string[]>{
+      var url = this.ocapUrlBuilderService.buildUrl('api/WorkstationTracker/GetWorkstationShortNames');
+      return this.httpClient.post<string[]>(url, workstationTrackerData, {
+        headers: this.ocapHttpHeadersService.getHeaders()
+      });
+    }
+
+    public GetAllWorkstationTrackerData(): Observable<string[]>{
+      var url = this.ocapUrlBuilderService.buildUrl('api/WorkstationTracker/GetAllWorkstationTrackerData');
+      return this.httpClient.post<string[]>(url, {
+        headers: this.ocapHttpHeadersService.getHeaders()
+      });
+    }
+
+    public Track(workstationTrackerData: WorkstationTrackerData): Observable<string[]>{
+      var url = this.ocapUrlBuilderService.buildUrl('api/WorkstationTracker/Track');
+      return this.httpClient.post<string[]>(url, workstationTrackerData, {
+        headers: this.ocapHttpHeadersService.getHeaders()
+      });
+    }
+
+    public UnTrack(workstationTrackerData: WorkstationTrackerData): Observable<string[]>{
+      var url = this.ocapUrlBuilderService.buildUrl('api/WorkstationTracker/UnTrack');
+      return this.httpClient.post<string[]>(url, workstationTrackerData, {
+        headers: this.ocapHttpHeadersService.getHeaders()
+      });
+    }
 }
