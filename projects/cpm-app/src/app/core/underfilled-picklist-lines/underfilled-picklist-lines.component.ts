a577c6ce2399c9f4e3ad010c32764f7b6c59c5b2
warren <warren.junker@omnicell.com>
2020-10-21 10:45:25 -0400

    minor cleanup

--- projects/cpm-app/src/app/core/underfilled-picklist-lines/underfilled-picklist-lines.component.ts
+++ projects/cpm-app/src/app/core/underfilled-picklist-lines/underfilled-picklist-lines.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { UnderfilledPicklistLine } from '../model/underfilled-picklist-line';
 import { WindowService } from '../../shared/services/window-service';
 import { IColHeaderSortChanged } from '../../shared/events/i-col-header-sort-changed';
