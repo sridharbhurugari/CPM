54795453786eb78c6467c51a56e9f27edacf3a7e
Jeff Fircak <>
2020-11-18 11:48:52 -0500

    Corrected event usage

--- projects/cpm-app/src/app/core/internal-transfer-device-summaries-page/internal-transfer-device-summaries-page.component.ts
+++ projects/cpm-app/src/app/core/internal-transfer-device-summaries-page/internal-transfer-device-summaries-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IDeviceReplenishmentNeed } from '../../api-core/data-contracts/i-device-replenishment-need';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
 import { DeviceReplenishmentNeedsService } from '../../api-core/services/device-replenishment-needs.service';
 import { CoreEventConnectionService } from "../../api-core/services/core-event-connection.service";
@@ -12,14 +13,20 @@ import { CoreEventConnectionService } from "../../api-core/services/core-event-c
 })
 export class InternalTransferDeviceSummariesPageComponent implements OnInit {
   deviceNeeds$: Observable<IDeviceReplenishmentNeed[]>;
+  ngUnsubscribe = new Subject();
 
   constructor(
     coreEventConnectionService: CoreEventConnectionService,
     private wpfActionControllerService: WpfActionControllerService,
     private deviceReplenishmentNeedsService: DeviceReplenishmentNeedsService,
   ) {
-    coreEventConnectionService.refreshDeviceNeedsSubject.subscribe(message => this.onRefreshDeviceNeeds());
+    coreEventConnectionService.refreshDeviceNeedsSubject.pipe(takeUntil(this.ngUnsubscribe)).subscribe(message => this.onRefreshDeviceNeeds());
     this.loadDeviceNeeds();
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
   private onRefreshDeviceNeeds(): void {
