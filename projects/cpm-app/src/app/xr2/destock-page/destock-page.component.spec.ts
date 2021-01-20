589a1857daba1f9020ab41ebb211e7a11cb0f89a
warren <warren.junker@omnicell.com>
2021-01-20 14:53:58 -0500

    add missing provider to test

--- projects/cpm-app/src/app/xr2/destock-page/destock-page.component.spec.ts
+++ projects/cpm-app/src/app/xr2/destock-page/destock-page.component.spec.ts
@@ -1,10 +1,8 @@
 import { EventEmitter, NO_ERRORS_SCHEMA, Output } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { TranslateService } from '@ngx-translate/core';
-
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable,  of, Subject  } from 'rxjs';
-import { finalize, catchError, map, shareReplay, tap } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
+import { finalize, catchError, map, shareReplay, tap, takeUntil } from 'rxjs/operators';
 import { IDestockTypeInfo } from '../../api-xr2/data-contracts/i-destock-type-info';
 import { DestockService } from '../../api-xr2/services/destock.service';
 import { nameof } from '../../shared/functions/nameof';
@@ -13,12 +11,13 @@ import { IXr2QueuePageConfiguration } from '../../shared/interfaces/i-xr2-queue-
 import { SelectableDeviceInfo } from '../../shared/model/selectable-device-info';
 import { DestockTypeInfo } from '../model/destock-type-info';
 import { SimpleDialogService } from '../../shared/services/dialogs/simple-dialog.service';
-import { DestockPageComponent } from './destock-page.component';
-import { SvgiconComponent, SvgIconModule, ProgressAnimationComponent, ButtonActionModule, FooterModule, GridModule, LayoutModule, SearchModule } from '@omnicell/webcorecomponents';
-import { MockDestockHeaderComponent } from '../../shared/testing/mock-destock-header-component.spec';
-import { MockDestockTypeInfoComponent } from '../../shared/testing/mock-destock-typeinfo-component.spec';
 import { DestockEventConnectionService } from '../services/destock-event-connection.service';
 import { DestockDataEvent } from '../model/destock-data-event';
+import { TranslateService } from '@ngx-translate/core';
+import { ProgressAnimationComponent } from '@omnicell/webcorecomponents';
+import { MockDestockHeaderComponent } from '../../shared/testing/mock-destock-header-component.spec';
+import { MockDestockTypeInfoComponent } from '../../shared/testing/mock-destock-typeinfo-component.spec';
+import { DestockPageComponent } from './destock-page.component';
 
 describe('DestockPageComponent', () => {
   let component: DestockPageComponent ;
@@ -26,6 +25,7 @@ describe('DestockPageComponent', () => {
   let translateService: Partial<TranslateService>;
   let simpleDialogService: Partial<SimpleDialogService>;
   let destockService: Partial<DestockService>;
+  let deviceDestockTypeInfo: Partial<DestockTypeInfo[]>;
   let destockEventConnectionService: Partial<DestockEventConnectionService>;
 
   beforeEach(async(() => {
@@ -54,6 +54,7 @@ describe('DestockPageComponent', () => {
       providers: [
         { provide: SimpleDialogService, useValue: simpleDialogService },
         { provide: DestockService, useValue: destockService},
+        { provide: DestockEventConnectionService, useValue: destockEventConnectionService}
        ]
     })
     .compileComponents();
