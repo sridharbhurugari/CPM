939c265582b1b65e95dc6fffaf27f658ab6d3d27
omnicell\josephr <joseph.ross@omnicell.com>
2021-02-09 16:42:12 -0500

    Removed unused properties from verification navigation

--- projects/cpm-app/src/app/shared/interfaces/i-verification-navigation-parameters.ts
+++ projects/cpm-app/src/app/shared/interfaces/i-verification-navigation-parameters.ts
@@ -2,10 +2,7 @@ import { VerificationRouting } from "../enums/verification-routing";
 
 export interface IVerificationNavigationParameters {
   DeviceId: number;
-  DeviceDescription: string;
   OrderId: string;
   DestinationId: string;
-  PriorityCodeDescription: string,
-  Date: Date,
   Route: VerificationRouting;
 }
