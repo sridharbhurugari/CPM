37d6d892b03045b6d10f390ab11af158f693e348
OMNICELL\JimS <jim.smith@omnicell.com>
2021-03-04 13:30:10 -0500

    Added test data

--- projects/cpm-app/src/app/core/internal-transfer-device-ondemand-item-location-list/internal-transfer-device-ondemand-item-location-list.component.spec.ts
+++ projects/cpm-app/src/app/core/internal-transfer-device-ondemand-item-location-list/internal-transfer-device-ondemand-item-location-list.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateService } from '@ngx-translate/core';
 import { LayoutModule, FooterModule, ButtonActionModule } from '@omnicell/webcorecomponents';
 import { of } from 'rxjs';
+import { IItemLocationDetail } from '../../api-core/data-contracts/i-item-location-detail';
+import { IItemReplenishmentOnDemand } from '../../api-core/data-contracts/i-item-replenishment-ondemand';
 import { MockColHeaderSortable } from '../../shared/testing/mock-col-header-sortable.spec';
 import { MockGridSortCol } from '../../shared/testing/mock-grid-sort-col.spec';
 import { MockAppHeaderContainer } from '../testing/mock-app-header.spec';
@@ -11,11 +13,28 @@ import { MockTranslatePipe } from '../testing/mock-translate-pipe.spec';
 
 import { InternalTransferDeviceOndemandItemLocationListComponent } from './internal-transfer-device-ondemand-item-location-list.component';
 
-describe('InternalTransferDeviceOndemandItemLocationListComponent', () => {
+fdescribe('InternalTransferDeviceOndemandItemLocationListComponent', () => {
   let component: InternalTransferDeviceOndemandItemLocationListComponent;
   let fixture: ComponentFixture<InternalTransferDeviceOndemandItemLocationListComponent>;
 
+  let selectedItemsData: IItemReplenishmentOnDemand = {
+    ItemId: "39301", ItemFormattedGenericName: "abacavir-lamivudine 600-300 mg TABLET", ItemBrandName: 'EPZICOM',
+    DeviceQuantityOnHand: 10, DeviceParLevel: 10, DeviceRestockLevel: 200, PendingDevicePickQuantity: 5,
+    DisplayPackageSize: '10', DisplayNumberOfPackages: '4', DisplayDeviceQuantityOnHand: "10", DisplayQohNumberOfPackages: '10', PackSize: 5, Xr2Item: true, UnitOfIssue: 'EA',
+    AvailablePharmacyLocationCount: 3, AvailablePharmacyQty: 20
+  };
+
+  let itemLocationsData: IItemLocationDetail[] = [{
+    ItemId: "I12345", ItemTradeName: "Item Trade Name", ItemFormattedGenericName: "Item Formatted Generic Name",
+    UnitOfIssue: "EA", QuantityOnHand: 139, ExpirationDate: "", ExpirationDateGranularity: null,
+    LocationDescription: "Location Description", DeviceLocationId: 123456, DeviceId: 1234, DeviceDescription: "Device Description",
+    DeviceType: "Device Type", PackageFormType: "Package Form Type", PackageFormName: "Package Form Name",
+    DeviceOwnerShortName: "Device Owner Short Name", DeviceDefaultOwner: null, CarouselDisplayQuantity: 139, Description: "Description",
+    Quantity: 0, DeviceLocation: null, SafetyStockIssueScan: false
+  }];
+
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
       declarations: [ InternalTransferDeviceOndemandItemLocationListComponent,
         MockTranslatePipe,
@@ -33,7 +52,7 @@ describe('InternalTransferDeviceOndemandItemLocationListComponent', () => {
       ],
       providers: [
         { provide: TranslateService, useValue: { get: () => of('') } },
-      ]
+      ],
     })
     .compileComponents();
   }));
@@ -41,6 +60,8 @@ describe('InternalTransferDeviceOndemandItemLocationListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InternalTransferDeviceOndemandItemLocationListComponent);
     component = fixture.componentInstance;
+    component.item = selectedItemsData;
+    component.itemLocations = itemLocationsData;
     fixture.detectChanges();
   });
 
