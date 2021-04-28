9891edc94d534de6ef227d765d5b8afc4de6a9d8
OMNICELL\WarrenJ <warren.junker@omnicell.com>
2021-04-28 16:40:10 -0400

    add unassigned service call

---
+++ projects/cpm-app/src/app/api-xr2/data-contracts/i-utilization-unassigned-medication-info-detail.ts
@@ -0,0 +1,6 @@
+export interface IUnassignedMedicationInfoDetail {
+  ItemCode: string;
+  PocketTypeId: number;
+  PocketTypeDescription: string;
+  Inventory: number;
+}
--- projects/cpm-app/src/app/xr2/services/utilization-details.service.ts
+++ projects/cpm-app/src/app/xr2/services/utilization-details.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { IErroredMedicationInfoDetail } from '../../api-xr2/data-contracts/i-utilization-errored-medication-info-detail';
+import { IErroredMedicationInfoDetail, IUnassignedMedicationInfoDetail } from '../../api-xr2/data-contracts/i-utilization-errored-medication-info-detail';
 import { Observable } from 'rxjs';
 import { OcapUrlBuilderService } from '../../shared/services/ocap-url-builder.service';
 import { OcapHttpHeadersService } from '../../shared/services/ocap-http-headers.service';
@@ -21,4 +21,13 @@ export class UtilizationDeailsService {
       headers: this.ocapHttpHeadersService.getHeaders()
     });
   }
+
+  public notAssigned(deviceId: number): Observable<IUnassignedMedicationInfoDetail[]>{
+      var encodedDeviceId = encodeURIComponent(deviceId);
+      var url = this.ocapUrlBuilderService.buildUrl(`/api/PocketUtilization/UnassignedMedDetails?deviceId=${encodedDeviceId}`);
+      return this.httpClient.get<IUnassignedMedicationInfoDetail[]>(url, {
+        headers: this.ocapHttpHeadersService.getHeaders()
+      });
+  }
+
 }
