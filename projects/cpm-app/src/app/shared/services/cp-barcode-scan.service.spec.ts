32a4f71ab8ade2719659d8119ad5a575791548f2
Milan Fatschel <milan.fatschel@omnicell.com>
2021-06-15 10:18:42 -0400

    Fixing test

--- projects/cpm-app/src/app/shared/services/cp-barcode-scan.service.spec.ts
+++ projects/cpm-app/src/app/shared/services/cp-barcode-scan.service.spec.ts
@@ -1,9 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { LoggerService } from 'oal-core';
 
 import { CpBarcodeScanService } from './cp-barcode-scan.service';
 
+const mockLoggerService = {
+  logInfo: (verbosity: any, category: any, message: any) => {},
+  logWarning: (verbosity: any, category: any, message: any) => {},
+  logError: (verbosity: any, category: any, message: any) => {}
+};
+
+
 describe('CpBarcodeScanService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: LoggerService, useValue: mockLoggerService },
+    ],
+  }));
 
   it('should be created', () => {
     const service: CpBarcodeScanService = TestBed.get(CpBarcodeScanService);
