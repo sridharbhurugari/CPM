9233fd7f6cdb85a5bd8bad8d8127c29dac2a3f1f
dave molesky <dmolesky04@gmail.com>
2021-06-24 10:48:18 -0400

    check in latest

--- projects/cpm-app/src/app/core/prepack-verification-selection/prepack-verification-selection.component.spec.ts
+++ projects/cpm-app/src/app/core/prepack-verification-selection/prepack-verification-selection.component.spec.ts
@@ -1,14 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PrepackVerificationSelectionComponent } from './prepack-verification-selection.component';
-/*
+import { MockCpClickableIconComponent } from "../../shared/testing/mock-cp-clickable-icon.spec";
+import { PrepackVerificationSelectionCacheService } from '../utilities/prepack-verification-selection-cache.service';
+import { of } from 'rxjs';
+import { GridModule } from '@omnicell/webcorecomponents';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MockTranslatePipe } from '../testing/mock-translate-pipe.spec';
+import { Router } from "@angular/router";
+import { PrepackVerificationQueueItem } from '../model/prepack-verification-queue-item';
+
 describe('PrepackVerificationSelectionComponent', () => {
   let component: PrepackVerificationSelectionComponent;
   let fixture: ComponentFixture<PrepackVerificationSelectionComponent>;
 
+  let prepackVerificationQueueItems = new Array<PrepackVerificationQueueItem>();
+  let prepackVerificationQueueItem = new PrepackVerificationQueueItem({
+    PrepackVerificationQueueId: 1,
+    ItemId: "test",
+    ItemDescription: "test",
+    DeviceId: 1,
+    DeviceDescription: "test",
+    QuantityToPackage: 2,
+    PackagedDate: new Date(),
+    DrugIdentifier: "test",
+    PrepackLotNumber: "test",
+    PrepackExpirationDate: new Date()
+   });
+   prepackVerificationQueueItems.push(prepackVerificationQueueItem);
+
+  let prepackVerificationSelectionCacheService: Partial<PrepackVerificationSelectionCacheService>;
+  prepackVerificationSelectionCacheService = {
+    Get: jasmine
+      .createSpy("Get")
+      .and.returnValue(of(prepackVerificationQueueItems))
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PrepackVerificationSelectionComponent ]
+      declarations: [ PrepackVerificationSelectionComponent,
+                      MockCpClickableIconComponent,
+                      MockTranslatePipe ],
+      imports: [GridModule],
+      schemas: [NO_ERRORS_SCHEMA],
+        providers: [
+          { provide: PrepackVerificationSelectionCacheService, useValue: prepackVerificationSelectionCacheService },
+          { provide: Router, useValue: { navigate: () => { } } },
+        ]
     })
     .compileComponents();
   }));
@@ -23,4 +61,4 @@ describe('PrepackVerificationSelectionComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-*/
+
