897fa9cb242c9b6567eb7dea23f3eebf0a83050d
Brian Albaugh <Brian.Albaugh@omnicell.com>
2021-07-27 23:35:00 -0400

    adding routing from invoices to tray create and edit

--- projects/cpm-app/src/app/core/constants/wpf-action-paths.ts
+++ projects/cpm-app/src/app/core/constants/wpf-action-paths.ts
@@ -2,4 +2,6 @@ export class WpfActionPaths{
   static ManualCycleCountPath: string = 'ManualCycleCountPath';
   static AdjustQohPath: string = 'AdjustQOH';
   static HighPriorityPickNow: string = 'HighPriorityPickNow'; 
+  static XR2AddTrayPath: string = 'XR2AddTrayPath';
+  static XR2EditTrayPath: string = 'XR2EditTrayPath';
 }
--- projects/cpm-app/src/app/xr2/xr2-invoices-page/xr2-invoices-page.component.ts
+++ projects/cpm-app/src/app/xr2/xr2-invoices-page/xr2-invoices-page.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { WpfActionPaths } from '../../core/constants/wpf-action-paths';
+import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
 
 @Component({
   selector: 'app-xr2-stocking-page',
   templateUrl: './xr2-invoices-page.component.html',
   styleUrls: ['./xr2-invoices-page.component.scss']
 })
-export class Xr2InvoicesPageComponent implements OnInit {
+export class Xr2InvoicesPageComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private wpfActionControllerService: WpfActionControllerService) { }
+  
+  ngAfterViewInit(): void {
+
+  }
 
   ngOnInit() {
   }
 
+  navigateExisting() {
+    let RestockTray =  {
+      DeviceId: 2,
+      IsReturn: false,
+      RestockTrayStatus: 0,
+      TrayId: 11,
+      IsInvoiceTray: true,
+    } as IRestockTray;
+
+    this.wpfActionControllerService.ExecuteActionNameWithData(WpfActionPaths.XR2EditTrayPath, { RestockTray });
+  }
+
+  navigateNew(){
+    let RestockTray =  {
+      DeviceId: 2,
+      IsReturn: false,
+      RestockTrayStatus: 0,
+      TrayId: 11,
+      IsInvoiceTray: true,
+    } as IRestockTray;
+
+    this.wpfActionControllerService.ExecuteActionNameWithData(WpfActionPaths.XR2AddTrayPath, { RestockTray });
+  }
+
+}
+
+export interface IRestockTray {
+  DeviceId: number,
+  IsReturn: boolean,
+  RestockTrayStatus: number,
+  TrayId: number,
+  IsInvoiceTray: boolean,
 }
