28302d25a7b648308d2cd51e727d132bf6c2c216
Brian Albaugh <Brian.Albaugh@omnicell.com>
2021-07-30 09:03:52 -0400

    undoing changes

--- projects/cpm-app/src/app/xr2/xr2-invoices-page/xr2-invoices-page.component.ts
+++ projects/cpm-app/src/app/xr2/xr2-invoices-page/xr2-invoices-page.component.ts
@@ -1,53 +1,15 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { WpfActionPaths } from '../../core/constants/wpf-action-paths';
-import { WpfActionControllerService } from '../../shared/services/wpf-action-controller/wpf-action-controller.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-xr2-stocking-page',
   templateUrl: './xr2-invoices-page.component.html',
   styleUrls: ['./xr2-invoices-page.component.scss']
 })
-export class Xr2InvoicesPageComponent implements OnInit, AfterViewInit {
+export class Xr2InvoicesPageComponent implements OnInit {
 
-  constructor(private wpfActionControllerService: WpfActionControllerService) { }
-  
-  ngAfterViewInit(): void {
-
-  }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  navigateExisting() {
-    let RestockTray =  {
-      DeviceId: 2,
-      IsReturn: false,
-      RestockTrayStatus: 0,
-      TrayId: 11,
-      IsInvoiceTray: true,
-    } as IRestockTray;
-
-    this.wpfActionControllerService.ExecuteActionNameWithData(WpfActionPaths.XR2EditTrayPath, { RestockTray });
-  }
-
-  navigateNew(){
-    let RestockTray =  {
-      DeviceId: 2,
-      IsReturn: false,
-      RestockTrayStatus: 0,
-      TrayId: 11,
-      IsInvoiceTray: true,
-    } as IRestockTray;
-
-    this.wpfActionControllerService.ExecuteActionNameWithData(WpfActionPaths.XR2AddTrayPath, { RestockTray });
-  }
-
-}
-
-export interface IRestockTray {
-  DeviceId: number,
-  IsReturn: boolean,
-  RestockTrayStatus: number,
-  TrayId: number,
-  IsInvoiceTray: boolean,
 }
